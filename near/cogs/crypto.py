8bea4f53f4812b15179fdf9272220ba856c61fb5
ZeaCeR#5641 <36286877+hirusha-adi@users.noreply.github.com>
2021-11-22 23:24:41 +0530

    bug fix

--- near/cogs/crypto.py
+++ near/cogs/crypto.py
@@ -1,5 +1,6 @@
 import discord
 import requests
+from bs4 import BeautifulSoup
 from discord.ext import commands
 from near.database import get_embeds
 
@@ -16,7 +17,7 @@ class Crypto(commands.Cog):
         self.please_wait_emb.set_thumbnail(url=get_embeds.PleaseWait.THUMBNAIL)
         self.please_wait_emb.set_footer(text=get_embeds.PleaseWait.FOOTER)
 
-    @commands.command()
+    @commands.command(aliases=["btc"])
     async def bitcoin(self, ctx):
         loading_message = await ctx.send(embed=self.please_wait_emb)
 
@@ -227,6 +228,56 @@ class Crypto(commands.Cog):
                 url="https://cdn.discordapp.com/attachments/877796755234783273/879741815237017680/52.png")
             embed.add_field(name="USD", value=f"{usd}", inline=False)
             embed.add_field(name="EUR", value=f"{eur}", inline=True)
+            embed.set_footer(text=f"Requested by {ctx.author.name}")
+            await loading_message.delete()
+            await ctx.send(embed=embed)
+
+        except Exception as e:
+            embed3 = discord.Embed(title=get_embeds.ErrorEmbeds.TITLE,
+                                   description=get_embeds.ErrorEmbeds.DESCRIPTION, color=get_embeds.ErrorEmbeds.COLOR)
+            embed3.set_author(name=f"{self.client.user.name}",
+                              icon_url=f"{self.client.user.avatar_url}")
+            embed3.set_thumbnail(url=get_embeds.ErrorEmbeds.THUMBNAIL)
+            embed3.add_field(
+                name=get_embeds.ErrorEmbeds.FIELD_NAME, value=f"{e}", inline=False)
+            embed3.set_footer(text=f"Requested by {ctx.author.name}")
+            await loading_message.delete()
+            await ctx.send(embed=embed3)
+
+    @commands.command()
+    async def rvn(self, ctx):
+        loading_message = await ctx.send(embed=self.please_wait_emb)
+
+        try:
+            c = requests.get(
+                'https://www.coingecko.com/en/coins/ravencoin/usd')
+            soup = BeautifulSoup(c.content, "html.parser")
+            raven_coin_value = soup.find_all("span", {
+                "class": "no-wrap", "data-coin-symbol": "rvn", "data-target": "price.price"})[0].text
+
+            one_day_trading_vol = soup.find_all(
+                "span", {"class": "no-wrap", "data-target": "price.price", "data-no-decimal": "false"})[0].text
+
+            circulating_supply = str(soup.find_all("span", {
+                "class": "tw-text-gray-900 dark:tw-text-white tw-float-right tw-font-medium tw-mr-1"})[0].text).strip()
+
+            total_supply = str(soup.find_all("span", {
+                "class": "tw-text-gray-900 dark:tw-text-white tw-float-right tw-font-medium tw-mr-1"})[1].text).strip()
+
+            embed = discord.Embed(
+                title="Ravencoin", color=get_embeds.Common.COLOR)
+            embed.set_author(name=f"{self.client.user.name}",
+                             icon_url=f"{self.client.user.avatar_url}")
+            embed.set_thumbnail(
+                url="https://cdn.discordapp.com/attachments/881007500588089404/912395666578374686/ravencoin-rvn-logo.png")
+            embed.add_field(
+                name="USD", value=f"{raven_coin_value}", inline=False)
+            embed.add_field(name="24 Hour Trading Vol",
+                            value=f"{one_day_trading_vol}", inline=False)
+            embed.add_field(
+                name="Circulating Supply", value=f"{circulating_supply}", inline=False)
+            embed.add_field(name="Total Supply",
+                            value=f"{total_supply}", inline=False)
             embed.set_footer(text=f"Requested by {ctx.author.name}")
             await loading_message.delete()
             await ctx.send(embed=embed)
