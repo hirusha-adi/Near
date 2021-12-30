40b271687c3f17ff2032a5c6d67f4a8bbffed13d
ZeaCeR#5641 <36286877+hirusha-adi@users.noreply.github.com>
2021-12-29 20:31:36 -0800

    Update 2-install-bot.sh

--- others/2-install-bot.sh
+++ others/2-install-bot.sh
@@ -3,7 +3,7 @@ echo "Please enter the bot token; Ctrl+S and Ctrl+X - after 5 seconds"
 sleep 5s
 nano ./Near/near/database/main.json
 clear
-echo "You can now start the bot, run the commands below"
+echo "You can start the bot, run the commands below. but first, run the third script to setup lavalink for music"
 echo "cd Near"
 echo "python3 main.py"
 echo "Ctrl+Z"
