111dd74c1f14974a02c8c85fcff3eb96532b30da
ZeaCeR#5641 <36286877+hirusha-adi@users.noreply.github.com>
2022-01-05 14:28:31 +0530

    removed unwanted stuff

--- others/start.sh
+++ others/start.sh
@@ -2,9 +2,10 @@ mkdir NearBot
 cd NearBot
 curl "https://raw.githubusercontent.com/hirusha-adi/Near/main/others/1-dependencies.sh" >> "1-dependencies.sh"
 curl "https://raw.githubusercontent.com/hirusha-adi/Near/main/others/2-install-bot.sh" >> "2-install-bot.sh"
-curl "https://raw.githubusercontent.com/hirusha-adi/Near/main/others/3-install-lavalink.sh" >> "3-install-lavalink.sh"
+# curl "https://raw.githubusercontent.com/hirusha-adi/Near/main/others/3-install-lavalink.sh" >> "3-install-lavalink.sh"
 curl "https://raw.githubusercontent.com/hirusha-adi/Near/main/others/remove.sh" >> "remove.sh"
-chmod +x "1-dependencies.sh" "2-install-bot.sh" "3-install-lavalink.sh" "remove.sh"
+chmod +x "3-install-lavalink.sh"
+chmod +x "1-dependencies.sh" "2-install-bot.sh" "remove.sh"
 chdmod +x "./near/Lavalink/install.sh"
 echo "Installing to Lavalink to ./near/Lavalink/"
 ./near/Lavalink/install.sh
