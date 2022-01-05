6eaf37d8d9afe2c8a9a3c8a58afa4e565fff3f08
ZeaCeR#5641 <36286877+hirusha-adi@users.noreply.github.com>
2022-01-05 14:37:40 +0530

    gdrsgrdgdergedr

--- others/start.sh
+++ others/start.sh
@@ -1,11 +1,12 @@
-mkdir NearBot
-cd NearBot
 curl "https://raw.githubusercontent.com/hirusha-adi/Near/main/others/1-dependencies.sh" >> "1-dependencies.sh"
 curl "https://raw.githubusercontent.com/hirusha-adi/Near/main/others/2-install-bot.sh" >> "2-install-bot.sh"
-# curl "https://raw.githubusercontent.com/hirusha-adi/Near/main/others/3-install-lavalink.sh" >> "3-install-lavalink.sh"
 curl "https://raw.githubusercontent.com/hirusha-adi/Near/main/others/remove.sh" >> "remove.sh"
-chmod +x "3-install-lavalink.sh"
+
+# curl "https://raw.githubusercontent.com/hirusha-adi/Near/main/others/3-install-lavalink.sh" >> "3-install-lavalink.sh"
+# chmod +x "3-install-lavalink.sh"
+
+cd ..
 chmod +x "1-dependencies.sh" "2-install-bot.sh" "remove.sh"
-chdmod +x "./near/Lavalink/install.sh"
-echo "Installing to Lavalink to ./near/Lavalink/"
-./near/Lavalink/install.sh
+chdmod +x "./NearBot/near/Lavalink/install.sh"
+echo "Installing to Lavalink to ./NearBot/near/Lavalink/"
+./NearBot/near/Lavalink/install.sh
