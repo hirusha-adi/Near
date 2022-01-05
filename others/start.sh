d111e691d064894aa493a447511256bdd27f63f4
ZeaCeR#5641 <36286877+hirusha-adi@users.noreply.github.com>
2022-01-05 14:45:23 +0530

    fdgdrgdr

--- others/start.sh
+++ others/start.sh
@@ -2,6 +2,4 @@ curl "https://raw.githubusercontent.com/hirusha-adi/Near/main/others/1-dependenc
 curl "https://raw.githubusercontent.com/hirusha-adi/Near/main/others/2-install-bot.sh" >> "2-install-bot.sh"
 curl "https://raw.githubusercontent.com/hirusha-adi/Near/main/others/remove.sh" >> "remove.sh"
 curl "https://raw.githubusercontent.com/hirusha-adi/Near/main/others/3-install-lavalink.sh" >> "3-install-lavalink.sh"
-pwd
 chmod +x "1-dependencies.sh" "2-install-bot.sh" "remove.sh" "3-install-lavalink.sh"
-chmod +x "./NearBot/near/Lavalink/install.sh"
