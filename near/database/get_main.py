61a4f8d6df5a5510800037225fa5fd1a4b6bf12d
Hirusha Adikari <36286877+hirusha-adi@users.noreply.github.com>
2022-07-28 19:55:39 +0530

    getters and setters for main settings

--- near/database/get_main.py
+++ near/database/get_main.py
@@ -1,12 +1,81 @@
 import json
+import typing as t
 
 
 class BotMainDB:
-    with open("near/database/main.json", "r", encoding="utf-8") as file:
-        embed = json.load(file)
 
-    MESSAGE_PREFIX = embed["MESSAGE_PREFIX"]
-    BOT_CREATOR_NAME = embed["BOT_CREATOR_NAME"]
-    BOT_VERSION = embed["BOT_VERSION"]
-    DEV_ID = embed["DEV_ID"]
-    DEV_AND_OWNERS = embed["DEV_AND_OWNERS"]
+    def __init__(self) -> None:
+        self._main_json = "near/database/main.json"
+        with open(self._main_json, "r", encoding="utf-8") as file:
+            self._embed = json.load(file)
+
+    @property
+    def MESSAGE_PREFIX(self):
+        return self._embed["MESSAGE_PREFIX"]
+
+    @MESSAGE_PREFIX.setter
+    def MESSAGE_PREFIX(self, value: t.Any):
+        if not(len(value) > 8):
+            value = str(value)
+        else:
+            value = "."
+        self._embed["MESSAGE_PREFIX"] = value
+        with open(self._main_json, "w", encoding="utf-8") as file:
+            self._embed = json.dump(self._embed, file)
+
+    @property
+    def BOT_CREATOR_NAME(self):
+        return self._embed["BOT_CREATOR_NAME"]
+
+    @BOT_CREATOR_NAME.setter
+    def BOT_CREATOR_NAME(self, value: t.Any):
+        self._embed["BOT_CREATOR_NAME"] = str(value)
+        with open(self._main_json, "w", encoding="utf-8") as file:
+            self._embed = json.dump(self._embed, file)
+
+    @property
+    def BOT_VERSION(self):
+        return self._embed["BOT_VERSION"]
+
+    @BOT_VERSION.setter
+    def BOT_VERSION(self, value: t.Any):
+        value = str(value)
+        if value.lower().startswith('v'):
+            final = value
+        else:
+            final = 'v' + value
+        self._embed["BOT_VERSION"] = final
+        with open(self._main_json, "w", encoding="utf-8") as file:
+            self._embed = json.dump(self._embed, file)
+
+    @property
+    def DEV_ID(self):
+        return self._embed["DEV_ID"]
+
+    @DEV_ID.setter
+    def DEV_ID(self, value: t.Any):
+        # will raise ValueError by default if not proper int
+        self._embed["DEV_ID"] = int(value)
+        with open(self._main_json, "w", encoding="utf-8") as file:
+            self._embed = json.dump(self._embed, file)
+
+    @property
+    def DEV_AND_OWNERS(self):
+        return self._embed["DEV_AND_OWNERS"]
+
+    @DEV_AND_OWNERS.setter
+    def DEV_AND_OWNERS(self, value: t.Any):
+        if "," in str(value):
+            values = str(value).split(',')
+            for i in values:
+                try:
+                    self._embed["DEV_AND_OWNERS"].append(int(values))
+                except ValueError:
+                    continue
+        else:
+            try:
+                self._embed["DEV_AND_OWNERS"].append(int(value))
+            except ValueError:
+                pass
+        with open(self._main_json, "w", encoding="utf-8") as file:
+            self._embed = json.dump(self._embed, file)
