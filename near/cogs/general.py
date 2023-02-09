cede864b4e7315d2d50012b51276881b84418357
Hirusha Adikari <36286877+hirusha-adi@users.noreply.github.com>
2023-02-09 11:08:44 +0530

    slash commands

--- near/cogs/general.py
+++ near/cogs/general.py
@@ -5,6 +5,7 @@ from time import time as nowtime
 import discord
 from discord.ext import commands
 from near.database import get_embeds, get_main
+from discord import app_commands
 
 
 class General(commands.Cog):
@@ -20,14 +21,6 @@ class General(commands.Cog):
         # Bot uptime
         self.start_time = None
 
-        # This is the please-wait/Loading embed
-        self.please_wait_emb = discord.Embed(
-            title=get_embeds.PleaseWait.TITLE, description=get_embeds.PleaseWait.DESCRIPTION, color=get_embeds.PleaseWait.COLOR)
-        self.please_wait_emb.set_author(
-            name=get_embeds.PleaseWait.AUTHOR_NAME, icon_url=get_embeds.PleaseWait.AUTHOR_URL)
-        self.please_wait_emb.set_thumbnail(url=get_embeds.PleaseWait.THUMBNAIL)
-        self.please_wait_emb.set_footer(text=get_embeds.PleaseWait.FOOTER)
-
     @commands.Cog.listener()
     async def on_ready(self):
         print(f'Logged in as {self.client.user.name}')
@@ -35,6 +28,9 @@ class General(commands.Cog):
         print(f'Python version: {cur_python_version()}')
         self.start_time = nowtime()
         await self.client.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name=f"teamsds.net/discord"))
+        synced = await self.client.tree.sync()
+        print(synced)
+        print(f'Synced {len(synced)} Slash Commands')
         print('Bot is ready!')
 
     @commands.Cog.listener()
@@ -87,9 +83,8 @@ class General(commands.Cog):
                             value=f"{message.content}", inline=False)
             await channel.send(embed=embed)
 
-    @commands.command()
-    async def ping(self, ctx):
-        loading_message = await ctx.send(embed=self.please_wait_emb)
+    @app_commands.command(name="ping", description="Check the response time of the Discord Bot")
+    async def ping(self, interaction: discord.Interaction):
         try:
             embed = discord.Embed(title="Response Time",
                                   color=get_embeds.Common.COLOR)
@@ -99,9 +94,8 @@ class General(commands.Cog):
                 url="https://cdn.discordapp.com/attachments/877796755234783273/879311068097290320/PngItem_1526969.png")
             embed.add_field(
                 name=f"Ping :timer:", value=f"{round(self.client.latency * 1000)} ms", inline=False)
-            embed.set_footer(text=f"Requested by {ctx.author.name}")
-            await loading_message.delete()
-            await ctx.send(embed=embed)
+            embed.set_footer(text=f"Requested by {interaction.user.name}")
+            await interaction.response.send_message(embed=embed)
 
         except Exception as e:
             embed3 = discord.Embed(title=get_embeds.ErrorEmbeds.TITLE,
@@ -111,14 +105,11 @@ class General(commands.Cog):
             embed3.set_thumbnail(url=get_embeds.ErrorEmbeds.THUMBNAIL)
             embed3.add_field(
                 name=get_embeds.ErrorEmbeds.FIELD_NAME, value=f"{e}", inline=False)
-            embed3.set_footer(text=f"Requested by {ctx.author.name}")
-            await loading_message.delete()
-            await ctx.send(embed=embed3)
+            embed3.set_footer(text=f"Requested by {interaction.user.name}")
+            await interaction.response.send_message(embed=embed3, ephemeral=True)
 
-    @commands.command()
-    async def uptime(self, ctx):
-        loading_message = await ctx.send(embed=self.please_wait_emb)
-
+    @app_commands.command(name="uptime", description="How long has the bot been up for?")
+    async def uptime(self, interaction: discord.Interaction):
         try:
             current_time = nowtime()
             difference = int(round(current_time - self.start_time))
@@ -126,9 +117,8 @@ class General(commands.Cog):
             embed = discord.Embed(color=get_embeds.Common.COLOR)
             embed.add_field(name="The bot was online for: ",
                             value=f":alarm_clock: {text}", inline=False)
-            embed.set_footer(text=f"Requested by {ctx.author.name}")
-            await loading_message.delete()
-            await ctx.send(embed=embed)
+            embed.set_footer(text=f"Requested by {interaction.user.name}")
+            await interaction.response.send_message(embed=embed)
 
         except Exception as e:
             embed3 = discord.Embed(title=get_embeds.ErrorEmbeds.TITLE,
@@ -138,26 +128,21 @@ class General(commands.Cog):
             embed3.set_thumbnail(url=get_embeds.ErrorEmbeds.THUMBNAIL)
             embed3.add_field(
                 name=get_embeds.ErrorEmbeds.FIELD_NAME, value=f"{e}", inline=False)
-            embed3.set_footer(text=f"Requested by {ctx.author.name}")
-            await loading_message.delete()
-            await ctx.send(embed=embed3)
+            embed3.set_footer(text=f"Requested by {interaction.user.name}")
+            await interaction.response.send_message(embed=embed3, ephemeral=True)
 
-    @commands.has_permissions(manage_messages=True)
-    @commands.command()
-    async def clean(self, ctx, amount=5):
-        """
-        Delete messages sent by the bot itself to the message channel
-        You need have the "manage_messages" permission to use this command
-        """
+    @app_commands.command(name="clean", description="Delete messages sent by the Bot")
+    @app_commands.describe(amount="Amount of messages to Delete")
+    @app_commands.checks.has_permissions(manage_messages=True)
+    async def clean(self, interaction: discord.Interaction, amount: int):
+        # interaction.user.guild_permissions.manage_channels
         try:
             if amount <= 100:
-                amttdel = amount + 1
-                await ctx.channel.purge(limit=amttdel, check=lambda m: m.author == self.client.user)
 
-                if amount == "1":
-                    msgtxt = "message"
-                else:
-                    msgtxt = "messages"
+                amttdel = amount + 1
+                await interaction.channel.purge(limit=amttdel, check=lambda m: m.author == self.client.user)
+
+                msgtxt = "message" if amount == "1" else "messages"
 
                 embed = discord.Embed(
                     title="Success!", color=get_embeds.Common.COLOR)
@@ -165,8 +150,9 @@ class General(commands.Cog):
                                  icon_url=f"{self.client.user.avatar_url}")
                 embed.add_field(
                     name="Action", value=f"Deleted {amount} {msgtxt} sent by {self.client.user.name}!", inline=False)
-                embed.set_footer(text=f"Requested by {ctx.author.name}")
-                await ctx.send(embed=embed, delete_after=4)
+                embed.set_footer(
+                    text=f"Requested by {interaction.user.name}")
+                await interaction.response.send_message(embed=embed, ephemeral=True)
 
             else:
                 embed2 = discord.Embed(title=get_embeds.ErrorEmbeds.TITLE,
@@ -176,8 +162,9 @@ class General(commands.Cog):
                 embed2.set_thumbnail(url=get_embeds.ErrorEmbeds.THUMBNAIL)
                 embed2.add_field(
                     name="Error:", value=f"Please enter a value below 100!", inline=False)
-                embed2.set_footer(text=f"Requested by {ctx.author.name}")
-                await ctx.send(embed=embed2)
+                embed2.set_footer(
+                    text=f"Requested by {interaction.user.name}")
+                await interaction.response.send_message(embed=embed2, ephemeral=True)
 
         except Exception as e:
             embed3 = discord.Embed(title=get_embeds.ErrorEmbeds.TITLE,
@@ -187,17 +174,17 @@ class General(commands.Cog):
             embed3.set_thumbnail(url=get_embeds.ErrorEmbeds.THUMBNAIL)
             embed3.add_field(
                 name=get_embeds.ErrorEmbeds.FIELD_NAME, value=f"{e}", inline=False)
-            embed3.set_footer(text=f"Requested by {ctx.author.name}")
-            await ctx.send(embed=embed3)
+            embed3.set_footer(text=f"Requested by {interaction.user.name}")
+            await interaction.response.send_message(embed=embed, ephemeral=True)
 
-    @commands.command()
-    async def help(self, ctx):
-        loading_message = await ctx.send(embed=self.please_wait_emb)
-        bp = get_main.BotMainDB.MESSAGE_PREFIX
+    @app_commands.command(name="help", description="Command Support")
+    async def help(self, interaction: discord.Interaction):
+
+        bp = "/"
 
         try:
             embed3 = discord.Embed(
-                title=":gear: Help", description="The list of all the commands! the might be some eastereggs!?! ", color=get_embeds.Common.COLOR)
+                title=":gear: Help", description="The list of all the commands! there might be some eastereggs!?! ", color=get_embeds.Common.COLOR)
             embed3.set_author(
                 name=f"{self.client.user.name}", icon_url=f"{self.client.user.avatar_url}")
             embed3.set_thumbnail(
@@ -214,23 +201,22 @@ class General(commands.Cog):
                 name="Music", value=f"`{bp}connect` - Connect to Voice Channel \n`{bp}disconnect` - Disconnect bot from Voice Channel \n`{bp}play [song-name/link]` - Play the song \n`{bp}skip` - Skip the currently playing song \n`{bp}pause` - Pause the music \n`{bp}resume` - Resume the music \n`{bp}seek [seconds]` - Skip the given seconds of the playing song \n`{bp}volume [number]` - Change the volume of the song \n`{bp}loop [type]` - Play music in a loop \n`{bp}nowplaying` - Show the song which is being played right now \n`{bp}queue` - Diplay the songs waiting to be played \n`{bp}equalizer` - Maybe tune the song to your liking?", inline=False)
 
             try:
-                if ctx.message.author.guild_permissions.manage_messages:
+                if interaction.user.guild_permissions.manage_messages:
                     embed3.add_field(
                         name="Manage", value=f"`{bp}clean [number_of_messages]` - Delete the given number of messages sent by the bot", inline=False)
             except:
                 pass
 
             try:
-                if ctx.author.id in get_main.BotMainDB.DEV_AND_OWNERS:
+                if interaction.user.id in get_main.BotMainDB.DEV_AND_OWNERS:
                     embed3.add_field(
                         name="Server Related", value=f"`{bp}av [@user or id]` - Get the profile picture of any user \n`{bp}serverinfo` - Show all publicly available information about the server \n`{bp}userinfo` - Show all the publicly available information about a user", inline=False)
             except:
                 pass
 
             embed3.add_field(name="Others", value=f"`{bp}countryinfo [country_code]` - Search for Country Information \n`{bp}hastebin [text]` - Create a hatebin link for the given text \n`{bp}insta [ig_username]` - Download the Instgram profile picture \n`{bp}ip [ip_addr]` - Find Information of an IP Address \n`{bp}lyrics [song_name]` - Find lyrics of any song \n`{bp}mfp [number]` - Mass fake profile \n`{bp}pwdcheck [password]` - Check for the status of a password \n`{bp}uptime` - Show bot uptime \n ", inline=False)
-            embed3.set_footer(text=f"Requested by {ctx.author.name}")
-            await loading_message.delete()
-            await ctx.send(embed=embed3)
+            embed3.set_footer(text=f"Requested by {interaction.user.name}")
+            await interaction.response.send_message(embed=embed3)
 
         except Exception as e:
             embed3 = discord.Embed(title=get_embeds.ErrorEmbeds.TITLE,
@@ -240,8 +226,8 @@ class General(commands.Cog):
             embed3.set_thumbnail(url=get_embeds.ErrorEmbeds.THUMBNAIL)
             embed3.add_field(
                 name=get_embeds.ErrorEmbeds.FIELD_NAME, value=f"{e}", inline=False)
-            embed3.set_footer(text=f"Requested by {ctx.author.name}")
-            await ctx.send(embed=embed3)
+            embed3.set_footer(text=f"Requested by {interaction.user.name}")
+            await interaction.response.send_message(embed=embed3, ephemeral=True)
 
 
 async def setup(client: commands.Bot):
