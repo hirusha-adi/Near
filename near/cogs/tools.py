f51be7c5faa82fe3089333938e4869fbeb5aac94
Hirusha Adikari <36286877+hirusha-adi@users.noreply.github.com>
2023-02-09 14:59:25 +0530

    add slash command support

--- near/cogs/tools.py
+++ near/cogs/tools.py
@@ -27,12 +27,12 @@ class Tools(commands.Cog):
         self.please_wait_emb.set_thumbnail(url=get_embeds.PleaseWait.THUMBNAIL)
         self.please_wait_emb.set_footer(text=get_embeds.PleaseWait.FOOTER)
 
-    @commands.command(aliases=["lyricsof"])
-    async def lyrics(self, ctx, *, search=None):
-        loading_message = await ctx.send(embed=self.please_wait_emb)
-
+    @app_commands.command(name="lyrics", description="Search the Lyrics of any Song")
+    @app_commands.describe(query="Name of the Song")
+    async def lyrics(self, intercation: discord.Interaction, query=None):
+        # Another Option: https://github.com/elmoiv/azapi
         try:
-            if not search:
+            if not query:
                 embed = discord.Embed(
                     title="No search argument!",
                     description="You havent entered anything, so i couldnt find lyrics!",
@@ -40,32 +40,20 @@ class Tools(commands.Cog):
                 )
                 embed.set_author(name=f"{self.client.user.name}",
                                  icon_url=f"{self.client.user.avatar_url}")
-                embed.set_footer(text=f"Requested by {ctx.author.name}")
-                try:
-                    await loading_message.delete()
-                except:
-                    pass
-                return await ctx.send(embed=embed)
+                embed.set_footer(text=f"Requested by {intercation.user.name}")
+                return await intercation.response.send_message(embed=embed)
 
-            song = urllib.parse.quote(search)
+            song = urllib.parse.quote(query)
 
             async with aiohttp.ClientSession() as lyricsSession:
                 async with lyricsSession.get(f'https://some-random-api.ml/lyrics?title={song}') as jsondata:
                     if not 300 > jsondata.status >= 200:
-                        try:
-                            await loading_message.delete()
-                        except:
-                            pass
-                        return await ctx.send(f'Recieved poor status code of {jsondata.status}')
+                        return await intercation.response.send_message(f'Recieved poor status code of {jsondata.status}')
                     lyricsData = await jsondata.json()
 
             error = lyricsData.get('error')
             if error:
-                try:
-                    await loading_message.delete()
-                except:
-                    pass
-                return await ctx.send(f'Recieved unexpected error: {error}')
+                return await intercation.response.send_message(f'Recieved unexpected error: {error}')
 
             songLyrics = lyricsData['lyrics']
             songArtist = lyricsData['author']
@@ -77,17 +65,13 @@ class Tools(commands.Cog):
                     title=f'{songTitle} - {songArtist}',
                     description=chunk,
                     color=get_embeds.Common.COLOR
-                    # timestamp = datetime.datetime.utcnow()
+                    # timestamp=datetime.datetime.utcnow()
                 )
                 embed.set_author(name=f"{self.client.user.name}",
                                  icon_url=f"{self.client.user.avatar_url}")
                 embed.set_thumbnail(url=songThumbnail)
-                embed.set_footer(text=f"Requested by {ctx.author.name}")
-                try:
-                    await loading_message.delete()
-                except:
-                    pass
-                await ctx.send(embed=embed)
+                embed.set_footer(text=f"Requested by {intercation.user.name}")
+                await intercation.response.send_message(embed=embed)
 
         except Exception as e:
             embed3 = discord.Embed(title=get_embeds.ErrorEmbeds.TITLE,
@@ -96,19 +80,17 @@ class Tools(commands.Cog):
                               icon_url=f"{self.client.user.avatar_url}")
             embed3.set_thumbnail(url=get_embeds.ErrorEmbeds.THUMBNAIL)
             embed3.add_field(name="Error:", value=f"{e}", inline=False)
-            embed3.set_footer(text=f"Requested by {ctx.author.name}")
-            await loading_message.delete()
-            await ctx.send(embed=embed3)
+            embed3.set_footer(text=f"Requested by {intercation.user.name}")
+            await intercation.response.send_message(embed=embed3)
 
-    @commands.command(aliases=["generate-pwd", "gen-pwd", "generate-password", "gen-password", "newpassword", "password", "newpass", "passwordnew"])
-    async def genpwd(self, ctx, *, numberofcharacters=16):
-        loading_message = await ctx.send(embed=self.please_wait_emb)
-
+    @app_commands.command(name='password_gen', description="Generate a very secure and unique password")
+    @app_commands.describe(lenght="Length of the Password to generate.")
+    async def genpwd(self, interaction: discord.Interaction, length: int):
         try:
 
-            if int(numberofcharacters) < 101:
+            if int(length) < 101:
                 c = ''.join((secrets.choice(string.ascii_letters +
-                            string.digits + string.punctuation) for i in range(int(numberofcharacters))))
+                            string.digits + string.punctuation) for i in range(int(length))))
 
                 embed = discord.Embed(
                     title="Password Generator", color=get_embeds.Common.COLOR)
@@ -117,12 +99,11 @@ class Tools(commands.Cog):
                 embed.set_thumbnail(
                     url="https://cdn.discordapp.com/attachments/877796755234783273/880031728369016832/704187.png")
                 embed.add_field(name="Password Length",
-                                value=f"{numberofcharacters}", inline=False)
+                                value=f"{length}", inline=False)
                 embed.add_field(name="Password",
                                 value=f"```{c}```", inline=False)
-                embed.set_footer(text=f"Requested by {ctx.author.name}")
-                await loading_message.delete()
-                await ctx.send(embed=embed)
+                embed.set_footer(text=f"Requested by {interaction.user.name}")
+                await interaction.response.send_message(embed=embed)
 
             else:
                 embed = discord.Embed(
@@ -135,9 +116,9 @@ class Tools(commands.Cog):
                     name="Error", value="The value of the number is high", inline=False)
                 embed.add_field(name="Possible Fix",
                                 value="Enter a value below 40", inline=False)
-                embed.set_footer(text=f"Requested by {ctx.author.name}")
-                await loading_message.delete()
-                await ctx.send(embed=embed)
+                embed.set_footer(
+                    text=f"Requested by {interaction.user.name}")
+                await interaction.response.send_message(embed=embed)
 
         except Exception as e:
             embed3 = discord.Embed(title=get_embeds.ErrorEmbeds.TITLE,
@@ -147,17 +128,16 @@ class Tools(commands.Cog):
             embed3.set_thumbnail(url=get_embeds.ErrorEmbeds.THUMBNAIL)
             embed3.add_field(
                 name=get_embeds.ErrorEmbeds.FIELD_NAME, value=f"{e}", inline=False)
-            embed3.set_footer(text=f"Requested by {ctx.author.name}")
-            await loading_message.delete()
-            await ctx.send(embed=embed3)
+            embed3.set_footer(text=f"Requested by {interaction.user.name}")
+            await interaction.response.send_message(embed=embed3)
 
-    @commands.command()
-    async def insta(self, ctx, *, ig_uname):
-        loading_message = await ctx.send(embed=self.please_wait_emb)
+    @app_commands.command(name="insta", description="Grab the Instagram Profile Picture of a Profile")
+    @app_commands.describe(text="Instagram Profile's Username")
+    async def insta(self, interaction: discord.Interaction, username: str):
         try:
             igpfp = instaloader.Instaloader()
-            igpfp.download_profile(ig_uname, profile_pic_only=True)
-            os.chdir(f'{ig_uname}')
+            igpfp.download_profile(username, profile_pic_only=True)
+            os.chdir(f'{username}')
             try:
                 os.system("mv *.jpg ..")
             except:
@@ -168,28 +148,27 @@ class Tools(commands.Cog):
             except:
                 os.system("ren *.jpg igtemp.jpg")
             try:
-                os.system(f'rm -r {ig_uname}')
+                os.system(f'rm -r {username}')
             except:
-                os.system(f'DEL {ig_uname} /F/Q/S')
+                os.system(f'DEL {username} /F/Q/S')
 
             file = discord.File(f'igtemp.jpg', filename="image.jpg")
             embed = discord.Embed(title="Instagram Profile Picture",
-                                  description=f"of {ig_uname}", color=get_embeds.Common.COLOR)
+                                  description=f"of {username}", color=get_embeds.Common.COLOR)
             embed.set_author(name=f"{self.client.user.name}",
                              icon_url=f"{self.client.user.avatar_url}")
             embed.add_field(
-                name="Link", value=f"https://instagram.com/{ig_uname}", inline=False)
+                name="Link", value=f"https://instagram.com/{username}", inline=False)
             embed.set_image(url="attachment://image.jpg")
-            embed.set_footer(text=f"Requested by {ctx.author.name}")
-            await loading_message.delete()
-            await ctx.send(file=file, embed=embed)
+            embed.set_footer(text=f"Requested by {interaction.user.name}")
+            await interaction.response.send_message(file=file, embed=embed)
 
             try:
                 os.system(f"rm igtemp.jpg")
             except:
-                os.remove(f'{ig_uname}')
+                os.remove(f'{username}')
         except Exception as e:
-            await ctx.send(f"Error: {e}")
+            await interaction.response.send_message(f"Error: {e}")
 
     @app_commands.command(name="bin", description="Create a PrivateBin from a Text")
     @app_commands.describe(text="Text to be included in the PrivateBin")
