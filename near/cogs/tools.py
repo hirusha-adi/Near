0df7131166bb865600212bacd6d3f34e2b333e33
Hirusha Adikari <36286877+hirusha-adi@users.noreply.github.com>
2023-02-09 15:21:18 +0530

    remove please wait message

--- near/cogs/tools.py
+++ near/cogs/tools.py
@@ -19,14 +19,6 @@ class Tools(commands.Cog):
     def __init__(self, client: commands.Bot):
         self.client = client
 
-        # This is the please-wait/Loading embed
-        self.please_wait_emb = discord.Embed(
-            title=get_embeds.PleaseWait.TITLE, description=get_embeds.PleaseWait.DESCRIPTION, color=get_embeds.PleaseWait.COLOR)
-        self.please_wait_emb.set_author(
-            name=get_embeds.PleaseWait.AUTHOR_NAME, icon_url=get_embeds.PleaseWait.AUTHOR_URL)
-        self.please_wait_emb.set_thumbnail(url=get_embeds.PleaseWait.THUMBNAIL)
-        self.please_wait_emb.set_footer(text=get_embeds.PleaseWait.FOOTER)
-
     @app_commands.command(name="lyrics", description="Search the Lyrics of any Song")
     @app_commands.describe(query="Name of the Song")
     async def lyrics(self, intercation: discord.Interaction, query=None):
