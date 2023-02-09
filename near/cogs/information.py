14f84b77dcc638abcd3f2b729fb74983c439efea
Hirusha Adikari <36286877+hirusha-adi@users.noreply.github.com>
2023-02-09 15:33:41 +0530

    add slash commands

--- near/cogs/information.py
+++ near/cogs/information.py
@@ -1,31 +1,25 @@
 import discord
 import requests
+from discord import app_commands
 from discord.ext import commands
-from near.database import get_embeds
 from zxcvbn import zxcvbn
+
+from near.database import get_embeds
 
 
 class Information(commands.Cog):
     def __init__(self, client: commands.Bot):
         self.client = client
 
-        # This is the please-wait/Loading embed
-        self.please_wait_emb = discord.Embed(
-            title=get_embeds.PleaseWait.TITLE, description=get_embeds.PleaseWait.DESCRIPTION, color=get_embeds.PleaseWait.COLOR)
-        self.please_wait_emb.set_author(
-            name=get_embeds.PleaseWait.AUTHOR_NAME, icon_url=get_embeds.PleaseWait.AUTHOR_URL)
-        self.please_wait_emb.set_thumbnail(url=get_embeds.PleaseWait.THUMBNAIL)
-        self.please_wait_emb.set_footer(text=get_embeds.PleaseWait.FOOTER)
-
         self.filepwdlist1 = open("near/assets/tenmilpwds.txt", "r")
         self.lines = self.filepwdlist1.readlines()
 
-    @commands.command(aliases=["ipinfo", "infoip", "ip-info", "info-ip"])
-    async def ip(self, ctx, *, ip_from_user):
-        loading_message = await ctx.send(embed=self.please_wait_emb)
+    @app_commands.command(name="ipinfo", description="IP Address Lookup")
+    @app_commands.describe(ip="IP Address to look for")
+    async def ipinfo(self, interaction: discord.Interaction, ip: str):
 
         try:
-            r = requests.get(f"https://ipapi.co/{ip_from_user}/json").json()
+            r = requests.get(f"https://ipapi.co/{ip}/json").json()
             rc = requests.get(
                 f"https://api.worldbank.org/v2/country/{r['country_code']}?format=json").json()
 
@@ -33,15 +27,14 @@ class Information(commands.Cog):
                                   color=get_embeds.Common.COLOR)
             embed.set_thumbnail(
                 url="https://user-images.githubusercontent.com/36286877/127773181-c98b63be-b18b-4d8b-a8b6-9426bd031b7c.png")
-            embed.set_footer(text=f"Requested by {ctx.author.name}")
+            embed.set_footer(text=f"Requested by {interaction.user.name}")
             embed.set_author(name=f"{self.client.user.name}",
                              icon_url=f"{self.client.user.avatar_url}")
             embed.add_field(name="IP Info", value="IP Address: " + str(r["ip"]) + "\nCity: " + str(r["city"]) + "\nRegion: " + str(r["region"]) + "\nCountry Name: " + str(r["country_name"]) + "\nLatitude: " + str(r["latitude"]) + "\nLongitude: " + str(r["longitude"]) + "\nTime Zone: " + str(r["timezone"]) + "\nUTC Offset: " + str(r["utc_offset"]) + "\nPostal Code: " + str(r["postal"]) + str("\nISP: " + r["org"]) + "\nASN: " + str(r["asn"]) + "\nCountry Code: " + str(
                 r["country_code"]) + "\nCountry TLD: " + str(r["country_tld"]) + "\nPopulation: " + str(r["country_population"]) + "\nCurrency: " + str(r["currency"]) + "\n Curreny Name: " + str(r["currency_name"]) + "\nCountry Area: " + str(r["country_area"]) + "\nLanguages: " + str(r["languages"]) + "\nCalling Code: " + str(r["country_calling_code"]) + "\nGOOGLE MAPS Link: " + f"https://maps.google.com/?q={r['latitude']},{r['longitude']}", inline=False)
             embed.add_field(name="Country Info", value="ID: " + str(rc[1][0]["id"]) + "\niso2Code: " + str(rc[1][0]["iso2Code"]) + "\nName" + str(rc[1][0]["name"]) + "\n\nRegion: " + "\n   ID: " + str(rc[1][0]["region"]["id"]) + "\n   iso2Code: " + str(rc[1][0]["region"]["iso2code"]) + "\n   Value: " + str(rc[1][0]["region"]["value"]) + "\n\nAdmin Region: " + "\n   ID: " + str(rc[1][0]["adminregion"]["id"]) + "\n   iso2Code: " + str(rc[1][0]["adminregion"]["iso2code"]) + "\n   Value: " + str(rc[1][0]["adminregion"]["value"]) + "\n\nIncome Level: " + "\n   ID: " + str(
                 rc[1][0]["incomeLevel"]["id"]) + "\n   iso2Code: " + str(rc[1][0]["incomeLevel"]["iso2code"]) + "\n   Value: " + str(rc[1][0]["incomeLevel"]["value"]) + "\n\nLending Type: " + "\n   ID: " + str(rc[1][0]["lendingType"]["id"]) + "\n   iso2Code: " + str(rc[1][0]["lendingType"]["iso2code"]) + "\n   Value: " + str(rc[1][0]["lendingType"]["value"]) + "\n\nCapital City: " + str(rc[1][0]["capitalCity"]) + "\nLongitude: " + str(rc[1][0]["longitude"]) + "\nLatitude: " + str(rc[1][0]["latitude"]), inline=False)
-            await loading_message.delete()
-            await ctx.send(embed=embed)
+            await interaction.response.send_message(embed=embed)
 
         except Exception as e:
             embed3 = discord.Embed(title=get_embeds.ErrorEmbeds.TITLE,
@@ -51,31 +44,26 @@ class Information(commands.Cog):
             embed3.set_thumbnail(url=get_embeds.ErrorEmbeds.THUMBNAIL)
             embed3.add_field(
                 name=get_embeds.ErrorEmbeds.FIELD_NAME, value=f"{e}", inline=False)
-            embed3.set_footer(text=f"Requested by {ctx.author.name}")
-            await loading_message.delete()
-            await ctx.send(embed=embed3)
+            embed3.set_footer(text=f"Requested by {interaction.user.name}")
+            await interaction.response.send_message(embed=embed3)
 
-    @commands.command(alises=["country-info", "country", "infocountry", "country-information"])
-    async def countryinfo(self, ctx, *, countrycodeig):
-        # MAKE SURE TO ENTER THE COUNTRY CODE AND NOT THE COUNTRY NAME
-        # eg- sg ( for Singapore ), us for ( United States )
-        loading_message = await ctx.send(embed=self.please_wait_emb)
-
+    @app_commands.command(name="countryinfo", description="Country Information Lookup")
+    @app_commands.describe(country="Country Code. Eg :- us, au, ca, sg, uk, nz")
+    async def countryinfo(self, interaction: discord.Interaction, country):
         try:
             rc = requests.get(
-                f"https://api.worldbank.org/v2/country/{countrycodeig}?format=json").json()
+                f"https://api.worldbank.org/v2/country/{country}?format=json").json()
 
             embed = discord.Embed(
                 title="Country Information", color=get_embeds.Common.COLOR)
             embed.set_thumbnail(
                 url="https://user-images.githubusercontent.com/36286877/129850352-33345963-273b-42bf-b2bc-5523c8158229.png")
-            embed.set_footer(text=f"Requested by {ctx.author.name}")
+            embed.set_footer(text=f"Requested by {interaction.user.name}")
             embed.set_author(name=f"{self.client.user.name}",
                              icon_url=f"{self.client.user.avatar_url}")
             embed.add_field(name="Country Info", value="ID: " + str(rc[1][0]["id"]) + "\niso2Code: " + str(rc[1][0]["iso2Code"]) + "\nName" + str(rc[1][0]["name"]) + "\n\nRegion: " + "\n   ID: " + str(rc[1][0]["region"]["id"]) + "\n   iso2Code: " + str(rc[1][0]["region"]["iso2code"]) + "\n   Value: " + str(rc[1][0]["region"]["value"]) + "\n\nAdmin Region: " + "\n   ID: " + str(rc[1][0]["adminregion"]["id"]) + "\n   iso2Code: " + str(rc[1][0]["adminregion"]["iso2code"]) + "\n   Value: " + str(rc[1][0]["adminregion"]["value"]) + "\n\nIncome Level: " + "\n   ID: " + str(
                 rc[1][0]["incomeLevel"]["id"]) + "\n   iso2Code: " + str(rc[1][0]["incomeLevel"]["iso2code"]) + "\n   Value: " + str(rc[1][0]["incomeLevel"]["value"]) + "\n\nLending Type: " + "\n   ID: " + str(rc[1][0]["lendingType"]["id"]) + "\n   iso2Code: " + str(rc[1][0]["lendingType"]["iso2code"]) + "\n   Value: " + str(rc[1][0]["lendingType"]["value"]) + "\n\nCapital City: " + str(rc[1][0]["capitalCity"]) + "\nLongitude: " + str(rc[1][0]["longitude"]) + "\nLatitude: " + str(rc[1][0]["latitude"]), inline=False)
-            await loading_message.delete()
-            await ctx.send(embed=embed)
+            await interaction.response.send_message(embed=embed)
 
         except Exception as e:
             embed3 = discord.Embed(title=get_embeds.ErrorEmbeds.TITLE,
@@ -85,16 +73,12 @@ class Information(commands.Cog):
             embed3.set_thumbnail(url=get_embeds.ErrorEmbeds.THUMBNAIL)
             embed3.add_field(
                 name=get_embeds.ErrorEmbeds.FIELD_NAME, value=f"{e}", inline=False)
-            embed3.set_footer(text=f"Requested by {ctx.author.name}")
-            await loading_message.delete()
-            await ctx.send(embed=embed3)
+            embed3.set_footer(text=f"Requested by {interaction.user.name}")
+            await interaction.response.send_message(embed=embed3)
 
-    @commands.command(aliases=["covidall"])
-    async def covid(self, ctx):
-        loading_message = await ctx.send(embed=self.please_wait_emb)
-
+    @app_commands.command(name="covid", description="Global Covid-19 Statistics")
+    async def covid(self, interaction: discord.Interaction):
         try:
-            # This uses the official API provided by the Sri Lankan Government to gather the needed data
             r = requests.get(
                 "https://www.hpb.health.gov.lk/api/get-current-statistical")
             c = r.json()
@@ -111,7 +95,7 @@ class Information(commands.Cog):
 
             em = discord.Embed(
                 title="COVID-19 Stats Global - All Info", color=get_embeds.Common.COLOR)
-            em.set_footer(text=f"Requested by {ctx.author.name}")
+            em.set_footer(text=f"Requested by {interaction.user.name}")
             em.set_author(name=f"{self.client.user.name}",
                           icon_url=f"{self.client.user.avatar_url}")
             em.set_thumbnail(
@@ -126,8 +110,7 @@ class Information(commands.Cog):
                          value=total_pcr_testing_count)
             em.add_field(name="Total Antigen Testing Count",
                          value=total_antigen_testing_count)
-            await loading_message.delete()
-            await ctx.send(embed=em)
+            await interaction.response.send_message(embed=em)
 
         except Exception as e:
             embed3 = discord.Embed(title=get_embeds.ErrorEmbeds.TITLE,
@@ -137,20 +120,14 @@ class Information(commands.Cog):
             embed3.set_thumbnail(url=get_embeds.ErrorEmbeds.THUMBNAIL)
             embed3.add_field(
                 name=get_embeds.ErrorEmbeds.FIELD_NAME, value=f"{e}", inline=False)
-            embed3.set_footer(text=f"Requested by {ctx.author.name}")
-            await loading_message.delete()
-            await ctx.send(embed=embed3)
+            embed3.set_footer(text=f"Requested by {interaction.user.name}")
+            await interaction.response.send_message(embed=embed3)
 
-    @commands.command(breif="Password Checker",
-                      description="This command will send you very useful information about your password",
-                      help="This command will send you very useful information about your password",
-                      aliases=['pwdcheck', 'pwdchk']
-                      )
-    async def passwordchk(self, ctx, *, password_inp):
-        loading_message = await ctx.send(embed=self.please_wait_emb)
-
+    @app_commands.command(name="passwordcheck", description="Password Strength Check and Profiler")
+    @app_commands.describe(password="Password to analyze")
+    async def passwordchk(self, interaction: discord.Interaction, password):
         try:
-            results = zxcvbn(f"{password_inp}")
+            results = zxcvbn(f"{password}")
             embed3 = discord.Embed(title="Password Check",
                                    description="using Low Budget Password Strength Estimation",
                                    color=get_embeds.Common.COLOR)
@@ -167,8 +144,11 @@ class Information(commands.Cog):
 
             embed3.add_field(
                 name="Guesses", value=f"Decimal - {results['guesses']}\nLog10 - {results['guesses_log10']}", inline=False)
-            # embed3.add_field(
-            #     name="Password", value=f"{results['password']}", inline=False)
+            try:
+                embed3.add_field(
+                    name="Password", value=f"{results['password']}", inline=False)
+            except:
+                pass
 
             sequence_info = ""
             for dic in results["sequence"]:
@@ -215,11 +195,9 @@ class Information(commands.Cog):
             except:
                 pass
 
-            embed3.set_footer(text=f"Requested by {ctx.author.name}")
+            embed3.set_footer(text=f"Requested by {interaction.user.name}")
 
-            await loading_message.delete()
-            # await ctx.send(f"```{results}```")
-            await ctx.send(embed=embed3)
+            await interaction.response.send_message(embed=embed3)
 
         except Exception as e:
             embed3 = discord.Embed(title=get_embeds.ErrorEmbeds.TITLE,
@@ -228,9 +206,8 @@ class Information(commands.Cog):
                               icon_url=f"{self.client.user.avatar_url}")
             embed3.set_thumbnail(url=get_embeds.ErrorEmbeds.THUMBNAIL)
             embed3.add_field(name="Error:", value=f"{e}", inline=False)
-            embed3.set_footer(text=f"Requested by {ctx.author.name}")
-            await loading_message.delete()
-            await ctx.send(embed=embed3)
+            embed3.set_footer(text=f"Requested by {interaction.user.name}")
+            await interaction.response.send_message(embed=embed3)
 
 
 async def setup(client: commands.Bot):
