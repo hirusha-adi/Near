efc1b2fd356641023e8f470c4dbe2c924924ec26
Hirusha Adikari <36286877+hirusha-adi@users.noreply.github.com>
2023-02-10 10:35:10 +0530

    buggy

--- near/cogs/musicplayer/music.py
+++ near/cogs/musicplayer/music.py
@@ -1,11 +1,15 @@
 import asyncio
 import re
+
 import discord
 import wavelink
-from .player import DisPlayer
+from discord import app_commands
 from discord.ext import commands
-from .checks import in_same_channel, player_connected, voice_connected
+
 from near.database import get_embeds
+
+from .checks import in_same_channel, player_connected, voice_connected
+from .player import DisPlayer
 
 
 class Music(commands.Cog, name="Music", description="Play any music easily!"):
@@ -41,63 +45,65 @@ class Music(commands.Cog, name="Music", description="Play any music easily!"):
                 except Exception as e:
                     print(e)
 
-    @commands.command(name="connect", aliases=["con", "c"])
+    @app_commands.command(name="connect", description="Connect to Voice Channel")
     @voice_connected()
-    async def connect_(self, ctx):
+    async def connect_(self, interaction: discord.Interaction):
         """Connect the player"""
         player: DisPlayer = self.bot.wavelink.get_player(
-            ctx.guild.id, cls=DisPlayer)
+            interaction.guild.id, cls=DisPlayer)
 
         if player.is_connected:
             if not player.bound_channel:
-                player.bound_channel = ctx.channel
+                player.bound_channel = interaction.channel
 
-            if player.channel_id == ctx.author.voice.channel.id:
-                return await ctx.send(
+            if player.channel_id == interaction.user.voice.channel.id:
+                return await interaction.response.send_message(
                     "Player is already connected to your voice channel."
                 )
 
-            return await ctx.send(
+            return await interaction.response.send_message(
                 f"Player is connected to a different voice channel. Can' join this."
             )
 
-        channel = ctx.author.voice.channel
-        self.bot.voice_users[ctx.author.id] = channel.id
+        channel = interaction.user.voice.channel
+        self.bot.voice_users[interaction.user.id] = channel.id
 
-        msg = await ctx.send(f"Connecting to **`{channel.name}`**")
+        msg = await interaction.response.send_message(f"Connecting to **`{channel.name}`**")
         await player.connect(channel.id)
-        player.bound_channel = ctx.channel
+        player.bound_channel = interaction.channel
         await msg.edit(
-            content=f"Connected to **`{channel.name}`** and bounded to {ctx.channel.mention}"
+            content=f"Connected to **`{channel.name}`** and bounded to {interaction.channel.mention}"
         )
 
-    @commands.command(name="disconnect", aliases=["dc", "leave"])
+    @app_commands.command(name="disconnect", description="Disconnect bot from Voice Channel")
     @voice_connected()
     @player_connected()
     @in_same_channel()
-    async def disconnect_(self, ctx):
+    async def disconnect_(self, interaction: discord.Interaction):
         """Destroy the player"""
         player: DisPlayer = self.bot.wavelink.get_player(
-            ctx.guild.id, cls=DisPlayer)
+            interaction.guild.id, cls=DisPlayer)
         await player.destroy()
 
-    @commands.command(name="play", aliases=["p"])
+    @app_commands.command(name="play", description="Play the song")
+    @app_commands.describe(query="Song name or URL")
     @voice_connected()
-    async def play_(self, ctx, *, query):
+    async def play_(self,  interaction: discord.Interaction, query: str):
         """Play or add song to queue"""
         player: DisPlayer = self.bot.wavelink.get_player(
-            ctx.guild.id, cls=DisPlayer)
+            interaction.guild.id, cls=DisPlayer)
 
         if not player.is_connected:
-            await ctx.invoke(self.connect_)
+            # BUG
+            await interaction.invoke(self.connect_)
 
-        if ctx.channel != player.bound_channel and player.bound_channel:
-            return await ctx.send(
+        if interaction.channel != player.bound_channel and player.bound_channel:
+            return await interaction.response.send_message(
                 f"Player is bounded to {player.bound_channel.mention}", delete_after=5
             )
-        player.bound_channel = ctx.channel
+        player.bound_channel = interaction.channel
 
-        msg = await ctx.send(f"Searching for `{query}` :mag_right:")
+        msg = await interaction.response.send_message(f"Searching for `{query}` :mag_right:")
         query = query.strip("<>")
         if not self.URL_REG.match(query):
             query = f"ytsearch:{query}"
@@ -122,17 +128,17 @@ class Music(commands.Cog, name="Music", description="Play any music easily!"):
         if not player.is_playing:
             await player.do_next()
 
-    @commands.command()
+    @app_commands.command(name="skip", description="Skip the currently playing song")
     @voice_connected()
     @player_connected()
     @in_same_channel()
-    async def skip(self, ctx):
+    async def skip(self, interaction: discord.Interaction):
         """Skip currently playing song"""
         player: DisPlayer = self.bot.wavelink.get_player(
-            ctx.guild.id, cls=DisPlayer)
+            interaction.guild.id, cls=DisPlayer)
 
-        if ctx.channel != player.bound_channel:
-            return await ctx.send(
+        if interaction.channel != player.bound_channel:
+            return await interaction.response.send_message(
                 f"Player is bounded to {player.bound_channel.mention}", delete_after=5
             )
 
@@ -144,63 +150,63 @@ class Music(commands.Cog, name="Music", description="Play any music easily!"):
         if current_loop != "CURRENT":
             player.loop = current_loop
 
-    @commands.command()
+    @app_commands.command(name="pause", description="Pause the music")
     @voice_connected()
     @player_connected()
     @in_same_channel()
-    async def pause(self, ctx):
+    async def pause(self, interaction: discord.Interaction):
         """Pause the player"""
         player: DisPlayer = self.bot.wavelink.get_player(
-            ctx.guild.id, cls=DisPlayer)
+            interaction.guild.id, cls=DisPlayer)
 
-        if ctx.channel != player.bound_channel:
-            return await ctx.send(
+        if interaction.channel != player.bound_channel:
+            return await interaction.response.send_message(
                 f"Player is bounded to {player.bound_channel.mention}", delete_after=5
             )
 
         if player.is_playing:
             if player.is_paused:
-                return await ctx.send("Player is already paused.")
+                return await interaction.response.send_message("Player is already paused.")
 
             await player.set_pause(pause=True)
-            return await ctx.send("Player is now paused.")
+            return await interaction.response.send_message("Player is now paused.")
 
-        await ctx.send("Player is not playing anything.")
+        await interaction.response.send_message("Player is not playing anything.")
 
-    @commands.command()
+    @app_commands.command(name="resume", description="Resume the music")
     @player_connected()
     @voice_connected()
     @in_same_channel()
-    async def resume(self, ctx):
+    async def resume(self, interaction: discord.Interaction):
         """Resume the player"""
         player: DisPlayer = self.bot.wavelink.get_player(
-            ctx.guild.id, cls=DisPlayer)
+            interaction.guild.id, cls=DisPlayer)
 
-        if ctx.channel != player.bound_channel:
-            return await ctx.send(
+        if interaction.channel != player.bound_channel:
+            return await interaction.response.send_message(
                 f"Player is bounded to {player.bound_channel.mention}", delete_after=5
             )
 
         if player.is_playing:
             if not player.is_paused:
-                return await ctx.send("Player is not paused.")
+                return await interaction.response.send_message("Player is not paused.")
 
             await player.set_pause(pause=False)
-            return await ctx.send("Player is now resumed.")
+            return await interaction.response.send_message("Player is now resumed.")
 
-        await ctx.send("Player is not playing anything.")
+        await interaction.response.send_message("Player is not playing anything.")
 
-    @commands.command()
+    @app_commands.command(name="seek", description="Skip the given seconds of the playing song")
     @voice_connected()
     @player_connected()
     @in_same_channel()
-    async def seek(self, ctx, seconds: int, reverse: bool = False):
+    async def seek(self, interaction: discord.Interaction, seconds: int, reverse: bool = False):
         """Seek the player backward or forward"""
         player: DisPlayer = self.bot.wavelink.get_player(
-            ctx.guild.id, cls=DisPlayer)
+            interaction.guild.id, cls=DisPlayer)
 
-        if ctx.channel != player.bound_channel:
-            return await ctx.send(
+        if interaction.channel != player.bound_channel:
+            return await interaction.response.send_message(
                 f"Player is bounded to {player.bound_channel.mention}", delete_after=5
             )
 
@@ -216,47 +222,47 @@ class Music(commands.Cog, name="Music", description="Play any music easily!"):
                         new_position = 0
 
                 await player.seek(new_position)
-                return await ctx.send(f"Player has been seeked {seconds} seconds.")
+                return await interaction.response.send_message(f"Player has been seeked {seconds} seconds.")
 
-            return await ctx.send(
+            return await interaction.response.send_message(
                 "Player is paused. Resume the player to use this command."
             )
 
-        await ctx.send("Player is not playing anything.")
+        await interaction.response.send_message("Player is not playing anything.")
 
-    @commands.command(aliases=["vol"])
+    @app_commands.command(name="volume", description="Change the volume of the song")
     @voice_connected()
     @player_connected()
     @in_same_channel()
-    async def volume(self, ctx, vol: int, forced=False):
+    async def volume(self, interaction: discord.Interaction, vol: int, forced=False):
         """Set volume"""
         player: DisPlayer = self.bot.wavelink.get_player(
-            ctx.guild.id, cls=DisPlayer)
+            interaction.guild.id, cls=DisPlayer)
 
-        if ctx.channel != player.bound_channel:
-            return await ctx.send(
+        if interaction.channel != player.bound_channel:
+            return await interaction.response.send_message(
                 f"Player is bounded to {player.bound_channel.mention}", delete_after=5
             )
 
         if vol < 0:
-            return await ctx.send("Volume can't be less than 0")
+            return await interaction.response.send_message("Volume can't be less than 0")
 
         if vol > 100 and not forced:
-            return await ctx.send("Volume can't greater than 100")
+            return await interaction.response.send_message("Volume can't greater than 100")
 
         await player.set_volume(vol)
 
-    @commands.command()
+    @app_commands.command(name="loop", description="Play music in a loop")
     @voice_connected()
     @player_connected()
     @in_same_channel()
-    async def loop(self, ctx, type: str = None):
+    async def loop(self, interaction: discord.Interaction, type: str = None):
         """Set loop to `NONE`, `CURRENT` or `PLAYLIST`"""
         player: DisPlayer = self.bot.wavelink.get_player(
-            ctx.guild.id, cls=DisPlayer)
+            interaction.guild.id, cls=DisPlayer)
 
-        if ctx.channel != player.bound_channel:
-            return await ctx.send(
+        if interaction.channel != player.bound_channel:
+            return await interaction.response.send_message(
                 f"Player is bounded to {player.bound_channel.mention}", delete_after=5
             )
 
@@ -277,56 +283,56 @@ class Music(commands.Cog, name="Music", description="Play any music easily!"):
             type = type.upper()
 
         if type not in valid_types:
-            return await ctx.send("Loop type must be `NONE`, `CURRENT` or `PLAYLIST`.")
+            return await interaction.response.send_message("Loop type must be `NONE`, `CURRENT` or `PLAYLIST`.")
 
         if len(player.queue._queue) < 1 and type == "PLAYLIST":
-            return await ctx.send(
+            return await interaction.response.send_message(
                 "There must be 2 songs in the queue in order to use the PLAYLIST loop"
             )
 
         if not player.is_playing:
-            return await ctx.send("Player is not playing any track. Can't loop")
+            return await interaction.response.send_message("Player is not playing any track. Can't loop")
 
         player.loop = type
 
-        await ctx.send(f"Player is now looping `{type}`")
+        await interaction.response.send_message(f"Player is now looping `{type}`")
 
-    @commands.command(aliases=["np"])
+    @app_commands.command(name="nowplaying", description="Show the song which is being played right now")
     @voice_connected()
     @player_connected()
     @in_same_channel()
-    async def nowplaying(self, ctx):
+    async def nowplaying(self, interaction: discord.Interaction):
         """What's playing now?"""
         player: DisPlayer = self.bot.wavelink.get_player(
-            ctx.guild.id, cls=DisPlayer)
+            interaction.guild.id, cls=DisPlayer)
 
-        if ctx.channel != player.bound_channel:
-            return await ctx.send(
+        if interaction.channel != player.bound_channel:
+            return await interaction.response.send_message(
                 f"Player is bounded to {player.bound_channel.mention}", delete_after=5
             )
 
         if not player.current:
-            return await ctx.send("Nothing is playing.")
+            return await interaction.response.send_message("Nothing is playing.")
 
         await player.invoke_player()
 
-    @commands.command(aliases=["q"])
+    @app_commands.command(name="queue", description="Diplay the songs waiting to be played")
     @voice_connected()
     @player_connected()
     @in_same_channel()
-    async def queue(self, ctx):
+    async def queue(self, interaction: discord.Interaction):
         """Player's current queue"""
         player: DisPlayer = self.bot.wavelink.get_player(
-            ctx.guild.id, cls=DisPlayer)
+            interaction.guild.id, cls=DisPlayer)
 
-        if ctx.channel != player.bound_channel:
-            return await ctx.send(
+        if interaction.channel != player.bound_channel:
+            return await interaction.response.send_message(
                 f"Player is bounded to {player.bound_channel.mention}", delete_after=5
             )
 
         queue = player.queue._queue
         if len(queue) < 1:
-            return await ctx.send("Nothing is in the queue.")
+            return await interaction.response.send_message("Nothing is in the queue.")
 
         embed = discord.Embed(color=get_embeds.Common.COLOR)
         embed.set_author(
@@ -346,19 +352,19 @@ class Music(commands.Cog, name="Music", description="Play any music easily!"):
 
         embed.description = tracks
 
-        await ctx.send(embed=embed)
+        await interaction.response.send_message(embed=embed)
 
-    @commands.command(aliases=["eq"])
+    @app_commands.command(name="equalizer", description="Maybe tune the song to your liking?")
     @voice_connected()
     @player_connected()
     @in_same_channel()
-    async def equalizer(self, ctx):
+    async def equalizer(self, interaction: discord.Interaction):
         """Set equalizer"""
         player: DisPlayer = self.bot.wavelink.get_player(
-            ctx.guild.id, cls=DisPlayer)
+            interaction.guild.id, cls=DisPlayer)
 
-        if ctx.channel != player.bound_channel:
-            return await ctx.send(
+        if interaction.channel != player.bound_channel:
+            return await interaction.response.send_message(
                 f"Player is bounded to {player.bound_channel.mention}", delete_after=5
             )
 
@@ -373,7 +379,7 @@ class Music(commands.Cog, name="Music", description="Play any music easily!"):
         embed.description = f"Current Eq - **{player.eq.name}**\n\n1. Flat \n2. Boost\n3. Metal\n4. Piano"
         embed.set_thumbnail(url="https://cdn.shahriyar.dev/equalizer.png")
 
-        msg = await ctx.send(embed=embed)
+        msg = await interaction.response.send_message(embed=embed)
 
         await msg.add_reaction("1️⃣")
         await msg.add_reaction("2️⃣")
@@ -383,7 +389,7 @@ class Music(commands.Cog, name="Music", description="Play any music easily!"):
         def check(reaction, user):
             return (
                 reaction.message.id == msg.id
-                and user.id == ctx.author.id
+                and user.id == interaction.user.id
                 and reaction.emoji in ["1️⃣", "2️⃣", "3️⃣", "4️⃣"]
             )
 
