e51aa0d80e4e95fa177a28d4e0aada2bee7d799f
Hirusha Adikari <36286877+hirusha-adi@users.noreply.github.com>
2023-02-11 00:07:20 +0530

    support input sanitization

--- near/cogs/endecoding.py
+++ near/cogs/endecoding.py
@@ -1,9 +1,12 @@
-import discord
 import base64
 import hashlib
+
+import discord
 from discord import app_commands
 from discord.ext import commands
+
 from near.database import get_embeds
+from near.utils import input_sanitization, errors
 
 
 class EncodeDecode(commands.Cog):
@@ -14,19 +17,21 @@ class EncodeDecode(commands.Cog):
     @app_commands.describe(text="Text to process")
     async def b64encode(self, interaction: discord.Interaction, text: str):
         try:
-            msg = base64.b64encode('{}'.format(text).encode('ascii'))
-            result = str(msg)
-            result = result[2:len(result)-1]
-            
-            embed = discord.Embed(title="Encode to Base64", color=get_embeds.Common.COLOR)
-            embed.set_author(name=f"{self.client.user.name}",icon_url=f"{self.client.user.avatar.url}")
-            embed.set_thumbnail(
-                url="https://cdn.discordapp.com/attachments/877796755234783273/879955815602200586/base64-logo-352x200.jpg")
-            embed.add_field(name="Query", value=f"{text}", inline=False)
-            embed.add_field(name="Result", value=f"{result}", inline=True)
-            embed.set_footer(text=f"Requested by {interaction.user.name}")
-
-            await interaction.response.send_message(embed=embed)
+            if input_sanitization.check_input(text):
+                msg = base64.b64encode('{}'.format(text).encode('ascii'))
+                result = str(msg)
+                result = result[2:len(result)-1]
+                
+                embed = discord.Embed(title="Encode to Base64", color=get_embeds.Common.COLOR)
+                embed.set_author(name=f"{self.client.user.name}",icon_url=f"{self.client.user.avatar.url}")
+                embed.set_thumbnail(
+                    url="https://cdn.discordapp.com/attachments/877796755234783273/879955815602200586/base64-logo-352x200.jpg")
+                embed.add_field(name="Query", value=f"{text}", inline=False)
+                embed.add_field(name="Result", value=f"{result}", inline=True)
+                embed.set_footer(text=f"Requested by {interaction.user.name}")
+                await interaction.response.send_message(embed=embed)
+            else:
+                raise errors.IllegalInput
 
         except Exception as e:
             embed3 = discord.Embed(title=get_embeds.ErrorEmbeds.TITLE,description=get_embeds.ErrorEmbeds.DESCRIPTION, color=get_embeds.ErrorEmbeds.COLOR)
@@ -35,26 +40,28 @@ class EncodeDecode(commands.Cog):
             embed3.add_field(name=get_embeds.ErrorEmbeds.FIELD_NAME, value=f"{e}", inline=False)
             embed3.set_footer(text=f"Requested by {interaction.user.name}")
 
-            await interaction.response.send_message(embed=embed3)
+            await interaction.response.send_message(embed=embed3, ephemeral=True)
 
     @app_commands.command(name="b64decode", description="Decode from Base64")
     @app_commands.describe(text="Text to process")
     async def b64decode(self, interaction: discord.Interaction, text: str):
 
         try:
+            if input_sanitization.is_base64(text):
+                msg = base64.b64decode('{}'.format(text).encode('ascii'))
+                result = str(msg)
+                result = result[2:len(result)-1]
+                embed = discord.Embed(title="Decode from Base64", color=get_embeds.Common.COLOR)
+                embed.set_author(name=f"{self.client.user.name}",icon_url=f"{self.client.user.avatar.url}")
+                embed.set_thumbnail(url="https://cdn.discordapp.com/attachments/877796755234783273/879955815602200586/base64-logo-352x200.jpg")
+                embed.add_field(name="Query", value=f"{text}", inline=False)
+                embed.add_field(name="Result", value=f"{result}", inline=True)
+                embed.set_footer(text=f"Requested by {interaction.user.name}")
 
-            msg = base64.b64decode('{}'.format(text).encode('ascii'))
-            result = str(msg)
-            result = result[2:len(result)-1]
-            embed = discord.Embed(title="Decode from Base64", color=get_embeds.Common.COLOR)
-            embed.set_author(name=f"{self.client.user.name}",icon_url=f"{self.client.user.avatar.url}")
-            embed.set_thumbnail(url="https://cdn.discordapp.com/attachments/877796755234783273/879955815602200586/base64-logo-352x200.jpg")
-            embed.add_field(name="Query", value=f"{text}", inline=False)
-            embed.add_field(name="Result", value=f"{result}", inline=True)
-            embed.set_footer(text=f"Requested by {interaction.user.name}")
-
-            await interaction.response.send_message(embed=embed)
-
+                await interaction.response.send_message(embed=embed)
+            else:
+                raise errors.IllegalInput
+                
         except Exception as e:
             embed3 = discord.Embed(title=get_embeds.ErrorEmbeds.TITLE,description=get_embeds.ErrorEmbeds.DESCRIPTION, color=get_embeds.ErrorEmbeds.COLOR)
             embed3.set_author(name=f"{self.client.user.name}",icon_url=f"{self.client.user.avatar.url}")
@@ -69,17 +76,20 @@ class EncodeDecode(commands.Cog):
     async def md5(self, interaction: discord.Interaction, text: str):
 
         try:
-            msg = hashlib.md5(text.encode())
-            slpake = msg.hexdigest()
+            if input_sanitization.check_input(text):
+                msg = hashlib.md5(text.encode())
+                slpake = msg.hexdigest()
 
-            embed = discord.Embed(title="to MD5", color=get_embeds.Common.COLOR)
-            embed.set_author(name=f"{self.client.user.name}",icon_url=f"{self.client.user.avatar.url}")
-            embed.set_thumbnail(url="https://cdn.discordapp.com/attachments/877796755234783273/879956672771137546/MD5.png")
-            embed.add_field(name="Query", value=f"{text}", inline=False)
-            embed.add_field(name="Result", value=f"{slpake}", inline=True)
-            embed.set_footer(text=f"Requested by {interaction.user.name}")
+                embed = discord.Embed(title="to MD5", color=get_embeds.Common.COLOR)
+                embed.set_author(name=f"{self.client.user.name}",icon_url=f"{self.client.user.avatar.url}")
+                embed.set_thumbnail(url="https://cdn.discordapp.com/attachments/877796755234783273/879956672771137546/MD5.png")
+                embed.add_field(name="Query", value=f"{text}", inline=False)
+                embed.add_field(name="Result", value=f"{slpake}", inline=True)
+                embed.set_footer(text=f"Requested by {interaction.user.name}")
 
-            await interaction.response.send_message(embed=embed)
+                await interaction.response.send_message(embed=embed)
+            else:
+                raise errors.IllegalInput
 
         except Exception as e:
             embed3 = discord.Embed(title=get_embeds.ErrorEmbeds.TITLE,description=get_embeds.ErrorEmbeds.DESCRIPTION, color=get_embeds.ErrorEmbeds.COLOR)
@@ -95,17 +105,20 @@ class EncodeDecode(commands.Cog):
     async def sha1(self, interaction: discord.Interaction, text: str):
 
         try:
-            msg = hashlib.sha1(text.encode())
-            slpuka = msg.hexdigest()
+            if input_sanitization.check_input(text):
+                msg = hashlib.sha1(text.encode())
+                slpuka = msg.hexdigest()
 
-            embed = discord.Embed(title="to SHA1", color=get_embeds.Common.COLOR)
-            embed.set_author(name=f"{self.client.user.name}",icon_url=f"{self.client.user.avatar.url}")
-            embed.set_thumbnail(url="https://cdn.discordapp.com/attachments/877796755234783273/879957622546108436/SHA1.png")
-            embed.add_field(name="Query", value=f"{text}", inline=False)
-            embed.add_field(name="Result", value=f"{slpuka}", inline=True)
-            embed.set_footer(text=f"Requested by {interaction.user.name}")
+                embed = discord.Embed(title="to SHA1", color=get_embeds.Common.COLOR)
+                embed.set_author(name=f"{self.client.user.name}",icon_url=f"{self.client.user.avatar.url}")
+                embed.set_thumbnail(url="https://cdn.discordapp.com/attachments/877796755234783273/879957622546108436/SHA1.png")
+                embed.add_field(name="Query", value=f"{text}", inline=False)
+                embed.add_field(name="Result", value=f"{slpuka}", inline=True)
+                embed.set_footer(text=f"Requested by {interaction.user.name}")
 
-            await interaction.response.send_message(embed=embed)
+                await interaction.response.send_message(embed=embed)
+            else:
+                raise errors.IllegalInput
 
         except Exception as e:
             embed3 = discord.Embed(title=get_embeds.ErrorEmbeds.TITLE,description=get_embeds.ErrorEmbeds.DESCRIPTION, color=get_embeds.ErrorEmbeds.COLOR)
@@ -121,17 +134,20 @@ class EncodeDecode(commands.Cog):
     async def sha224(self, interaction: discord.Interaction, text: str):
 
         try:
-            msg = hashlib.sha3_224(text.encode())
-            crnja = msg.hexdigest()
+            if input_sanitization.check_input(text):
+                msg = hashlib.sha3_224(text.encode())
+                crnja = msg.hexdigest()
 
-            embed = discord.Embed(title="to SHA224", color=get_embeds.Common.COLOR)
-            embed.set_author(name=f"{self.client.user.name}",icon_url=f"{self.client.user.avatar.url}")
-            embed.set_thumbnail(url="https://cdn.discordapp.com/attachments/877796755234783273/879958751640191046/download.png")
-            embed.add_field(name="Query", value=f"{text}", inline=False)
-            embed.add_field(name="Result", value=f"{crnja}", inline=True)
-            embed.set_footer(text=f"Requested by {interaction.user.name}")
+                embed = discord.Embed(title="to SHA224", color=get_embeds.Common.COLOR)
+                embed.set_author(name=f"{self.client.user.name}",icon_url=f"{self.client.user.avatar.url}")
+                embed.set_thumbnail(url="https://cdn.discordapp.com/attachments/877796755234783273/879958751640191046/download.png")
+                embed.add_field(name="Query", value=f"{text}", inline=False)
+                embed.add_field(name="Result", value=f"{crnja}", inline=True)
+                embed.set_footer(text=f"Requested by {interaction.user.name}")
 
-            await interaction.response.send_message(embed=embed)
+                await interaction.response.send_message(embed=embed)
+            else:
+                raise errors.IllegalInput
 
         except Exception as e:
             embed3 = discord.Embed(title=get_embeds.ErrorEmbeds.TITLE,description=get_embeds.ErrorEmbeds.DESCRIPTION, color=get_embeds.ErrorEmbeds.COLOR)
@@ -147,17 +163,20 @@ class EncodeDecode(commands.Cog):
     async def sha512(self, interaction: discord.Interaction, text: str):
 
         try:
-            msg = hashlib.sha3_512(text.encode())
-            crnja = msg.hexdigest()
+            if input_sanitization.check_input(text):
+                msg = hashlib.sha3_512(text.encode())
+                crnja = msg.hexdigest()
 
-            embed = discord.Embed(title="to SHA512", color=get_embeds.Common.COLOR)
-            embed.set_author(name=f"{self.client.user.name}",icon_url=f"{self.client.user.avatar.url}")
-            embed.set_thumbnail(url="https://cdn.discordapp.com/attachments/877796755234783273/879960296863698944/download_1.png")
-            embed.add_field(name="Query", value=f"{text}", inline=False)
-            embed.add_field(name="Result", value=f"{crnja}", inline=True)
-            embed.set_footer(text=f"Requested by {interaction.user.name}")
+                embed = discord.Embed(title="to SHA512", color=get_embeds.Common.COLOR)
+                embed.set_author(name=f"{self.client.user.name}",icon_url=f"{self.client.user.avatar.url}")
+                embed.set_thumbnail(url="https://cdn.discordapp.com/attachments/877796755234783273/879960296863698944/download_1.png")
+                embed.add_field(name="Query", value=f"{text}", inline=False)
+                embed.add_field(name="Result", value=f"{crnja}", inline=True)
+                embed.set_footer(text=f"Requested by {interaction.user.name}")
 
-            await interaction.response.send_message(embed=embed)
+                await interaction.response.send_message(embed=embed)
+            else:
+                raise errors.IllegalInput
 
         except Exception as e:
             embed3 = discord.Embed(title=get_embeds.ErrorEmbeds.TITLE,description=get_embeds.ErrorEmbeds.DESCRIPTION, color=get_embeds.ErrorEmbeds.COLOR)
@@ -173,17 +192,20 @@ class EncodeDecode(commands.Cog):
     async def leet(self, interaction: discord.Interaction, text: str):
 
         try:
-            encoded = text.replace('e', '3').replace('a', '4').replace('i', '!').replace('u', '|_|').replace('U', '|_|').replace('E', '3').replace('I', '!').replace('A', '4').replace('o', '0').replace(
-                'O', '0').replace('t', '7').replace('T', '7').replace('l', '1').replace('L', '1').replace('k', '|<').replace('K', '|<').replace('CK', 'X').replace('ck', 'x').replace('Ck', 'X').replace('cK', 'x')
+            if input_sanitization.check_input(text):
+                encoded = text.replace('e', '3').replace('a', '4').replace('i', '!').replace('u', '|_|').replace('U', '|_|').replace('E', '3').replace('I', '!').replace('A', '4').replace('o', '0').replace(
+                    'O', '0').replace('t', '7').replace('T', '7').replace('l', '1').replace('L', '1').replace('k', '|<').replace('K', '|<').replace('CK', 'X').replace('ck', 'x').replace('Ck', 'X').replace('cK', 'x')
 
-            embed = discord.Embed(title="to LEET", color=get_embeds.Common.COLOR)
-            embed.set_author(name=f"{self.client.user.name}",icon_url=f"{self.client.user.avatar.url}")
-            embed.set_thumbnail(url="https://cdn.discordapp.com/attachments/877796755234783273/879961162895212574/download_2.png")
-            embed.add_field(name="Query", value=f"{text}", inline=False)
-            embed.add_field(name="Result", value=f"{encoded}", inline=True)
-            embed.set_footer(text=f"Requested by {interaction.user.name}")
+                embed = discord.Embed(title="to LEET", color=get_embeds.Common.COLOR)
+                embed.set_author(name=f"{self.client.user.name}",icon_url=f"{self.client.user.avatar.url}")
+                embed.set_thumbnail(url="https://cdn.discordapp.com/attachments/877796755234783273/879961162895212574/download_2.png")
+                embed.add_field(name="Query", value=f"{text}", inline=False)
+                embed.add_field(name="Result", value=f"{encoded}", inline=True)
+                embed.set_footer(text=f"Requested by {interaction.user.name}")
 
-            await interaction.response.send_message(embed=embed)
+                await interaction.response.send_message(embed=embed)
+            else:
+                raise errors.IllegalInput
 
         except Exception as e:
             embed3 = discord.Embed(title=get_embeds.ErrorEmbeds.TITLE,description=get_embeds.ErrorEmbeds.DESCRIPTION, color=get_embeds.ErrorEmbeds.COLOR)
