0dcb3562bdf34f347676a6f29b40e78c7d4e89aa
Hirusha Adikari <36286877+hirusha-adi@users.noreply.github.com>
2023-02-10 23:58:00 +0530

    Update errors.py

--- near/utils/errors.py
+++ near/utils/errors.py
@@ -5,8 +5,3 @@ class IllegalInput(Exception):
     def __str__(self):
         return f"Illegal Input Passed to the bot as an argument{': ' + str(self.message) if not(self.message is None) else '.'}"
     
-    
-try:
-    raise IllegalInput
-except Exception as e:
-    print(e)
