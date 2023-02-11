5c80688ab6ea764e087a6b82971b703640806c00
Hirusha Adikari <36286877+hirusha-adi@users.noreply.github.com>
2023-02-11 06:26:30 +0530

    bug fix

--- near/cogs/musicplayer/__init__.py
+++ near/cogs/musicplayer/__init__.py
@@ -1,10 +1,11 @@
 from .music import Music
 from .events import MusicEvents
+from discord.ext import commands
 
 
-def setup(bot):
-    bot.add_cog(Music(bot))
-    bot.add_cog(MusicEvents(bot))
+async def setup(bot: commands.Bot):
+    await bot.add_cog(Music(bot))
+    await bot.add_cog(MusicEvents(bot))
 
 
 __version__ = "1.0.1"
