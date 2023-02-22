b613dc150ebb1a90b0670b3ec9d89540734a0b03
Hirusha Adikari <36286877+hirusha-adi@users.noreply.github.com>
2023-02-22 19:20:53 +0530

    support new version

--- near/cogs/musicplayer/__init__.py
+++ near/cogs/musicplayer/__init__.py
@@ -2,9 +2,9 @@ from .music import Music
 from .events import MusicEvents
 
 
-def setup(bot):
-    bot.add_cog(Music(bot))
-    bot.add_cog(MusicEvents(bot))
+async def setup(bot):
+    await bot.add_cog(Music(bot))
+    await bot.add_cog(MusicEvents(bot))
 
 
 __version__ = "1.0.1"
