1b93ae941824350e42f6abe1bd0c43398a8726e2
hirusha-adi <36286877+hirusha-adi@users.noreply.github.com>
2024-02-26 18:12:42 +0530

    create all 4 types of embeds and return them

--- near/utils/embeds.py
+++ near/utils/embeds.py
@@ -1,5 +1,8 @@
+import typing as t
+
 import discord
 from near.database.db import Embeds
+from near.utils.input_sanitization import color
 
 
 def Error(interaction: discord.Interaction, error_message: str) -> discord.Embed:
@@ -8,7 +11,7 @@ def Error(interaction: discord.Interaction, error_message: str) -> discord.Embed
     embed = discord.Embed(
         title=db_error["error_title"],
         description=db_error["error_description"],
-        color=0x0000FF,
+        color=color(db_error["error_color"]),
     )
     embed.set_author(
         name=db_common["common_author_name"], icon_url=db_common["common_author_url"]
@@ -18,4 +21,76 @@ def Error(interaction: discord.Interaction, error_message: str) -> discord.Embed
         name=db_error["error_feild_name"], value=f"{error_message}", inline=False
     )
     embed.set_footer(text=f"Requested by {interaction.user.name}")
+    return embed
+
+
+def Common(
+    interaction: discord.Interaction,
+    title: str,
+    description: str | list,
+    thumnail: t.Optional[str] = "",
+) -> discord.Embed:
+
+    db_common = Embeds.CommonEmbed()
+
+    embed = discord.Embed(
+        title=title,
+        description=(
+            "".join(description) if isinstance(description, list) else description
+        ),
+        color=color(db_common["common_color"]),
+    )
+    embed.set_author(
+        name=db_common["common_author_name"], icon_url=db_common["common_author_url"]
+    )
+    if thumnail:
+        embed.set_thumbnail(url=thumnail)
+    embed.set_footer(text=f"Requested by {interaction.user.name}")
+    return embed
+
+
+def Fake(
+    interaction: discord.Interaction,
+    description: str | list,
+) -> discord.Embed:
+
+    db_fake = Embeds.FakeEmbed()
+    db_common = Embeds.CommonEmbed()
+
+    embed = discord.Embed(
+        title=db_fake["fakeinfo_title"],
+        description=(
+            "".join(description) if isinstance(description, list) else description
+        ),
+        color=color(db_fake["fakeinfo_color"]),
+    )
+    embed.set_author(
+        name=db_common["common_author_name"], icon_url=db_common["common_author_url"]
+    )
+    embed.set_thumbnail(url=db_fake["fakeinfo_thumbnail"])
+    embed.set_footer(text=f"Requested by {interaction.user.name}")
+    return embed
+
+
+def PleaseWait(
+    interaction: discord.Interaction,
+) -> discord.Embed:
+
+    db_please_wait = Embeds.PleaseWaitEmbed()
+    db_common = Embeds.CommonEmbed()
+
+    embed = discord.Embed(
+        title=db_please_wait["pleasewait_title"],
+        description=db_please_wait["pleasewait_description"],
+        color=color(db_please_wait["pleasewait_color"]),
+    )
+    embed.set_author(
+        name=db_common["common_author_name"], icon_url=db_common["common_author_url"]
+    )
+    embed.set_thumbnail(url=db_please_wait["pleasewait_thumbnail"])
+    embed.set_footer(
+        text=str(db_please_wait["pleasewait_footer"]).format(
+            username=interaction.user.name
+        )
+    )
     return embed
