bc063f4c5cb786029e329d06ca42f611ab666321
hirusha-adi <36286877+hirusha-adi@users.noreply.github.com>
2024-02-26 15:22:31 +0530

    get embed info from code, optmized

--- near/database/db.py
+++ near/database/db.py
@@ -5,7 +5,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-CONFIG = {
+__CONFIG = {
     "host": os.getenv("PSQL_HOST"),
     "user": os.getenv("PSQL_USER"),
     "password": os.getenv("PSQL_PASSWORD"),
@@ -14,7 +14,7 @@ CONFIG = {
 
 
 def connect_to_postgres():
-    return psycopg2.connect(**CONFIG)
+    return psycopg2.connect(**__CONFIG)
 
 
 def postgres_connection(func):
@@ -56,6 +56,59 @@ class Embeds:
      pleasewait_title       | .....
     (16 rows)
     """
+
+    @staticmethod
+    def __create_table_embed(cursor):
+        """Create table if it does not exist"""
+        cursor.execute(
+            "SELECT EXISTS (SELECT FROM information_schema.tables WHERE table_name = 'embeds')"
+        )
+        table_exists = cursor.fetchone()[0]
+        if not table_exists:
+            cursor.execute(
+                "CREATE TABLE embeds (key VARCHAR(255) PRIMARY KEY, value TEXT);"
+            )
+            print("Table 'embeds' created successfully.")
+
+    @staticmethod
+    def __setup_table_embed():
+        """Load `embeds` table with defaults"""
+
+        # pleasewait table
+        Embeds.add("pleasewait_author_name", "NeaBot")
+        Embeds.add(
+            "pleasewait_author_url",
+            "https://cdn.discordapp.com/attachments/881007500588089404/881046764206039070/unknown.png",
+        )
+        Embeds.add("Pleasewait_title", "Please Wait")
+        Embeds.add("pleasewait_description", "``` Processing Your Request ```")
+        Embeds.add("pleasewait_color", "red")
+        Embeds.add(
+            "pleasewait_thumbnail",
+            "https://c.tenor.com/I6kN-6X7nhAAAAAj/loading-buffering.gif",
+        )
+        Embeds.add("pleasewait_footer", "Bot created by @hirushaadi")
+
+        # error
+        Embeds.add("error_title", "NeaBot")
+        Embeds.add("error_description", "The command was unable to run successfully!")
+        Embeds.add(
+            "error_thumbnail",
+            "https://cdn.discordapp.com/attachments/877796755234783273/879298565380386846/sign-red-error-icon-1.png",
+        )
+        Embeds.add("error_feild_name", "Error:")
+        Embeds.add("error_color", "red")
+
+        # fake embeds for `near.cogs.fakeinfo`
+        Embeds.add("fakeinfo_title", "Fake Information Generator")
+        Embeds.add(
+            "fakeinfo_thumbnail",
+            "https://www.nicepng.com/png/detail/214-2146883_4-fake-stamp-vector-fake-news-logo-png.png",
+        )
+        Embeds.add("fakeinfo_color", "red")
+
+        # common
+        Embeds.add("common_color", "red")
 
     @staticmethod
     @postgres_connection
@@ -102,78 +155,76 @@ class Embeds:
         except psycopg2.Error as error:
             print("Failed to delete record from PostgreSQL table:", error)
 
+    @staticmethod
+    @postgres_connection
+    def PleaseWait(connection):
+        cursor = connection.cursor()
+        sql_query = "SELECT key, value FROM embeds WHERE key LIKE 'pleasewait_%'"
+        cursor.execute(sql_query)
+        rows = cursor.fetchall()
+        embed_details = {}
+        for row in rows:
+            key, value = row
+            embed_details[key] = value
+        return embed_details
+
+    @staticmethod
+    @postgres_connection
+    def ErrorEmbeds(connection):
+        cursor = connection.cursor()
+        sql_query = "SELECT key, value FROM embeds WHERE key LIKE 'error_%'"
+        cursor.execute(sql_query)
+        rows = cursor.fetchall()
+        embed_details = {}
+        for row in rows:
+            key, value = row
+            embed_details[key] = value
+        return embed_details
+
+    @staticmethod
+    @postgres_connection
+    def Common(connection):
+        cursor = connection.cursor()
+        sql_query = "SELECT key, value FROM embeds WHERE key LIKE 'common_%'"
+        cursor.execute(sql_query)
+        rows = cursor.fetchall()
+        embed_details = {}
+        for row in rows:
+            key, value = row
+            embed_details[key] = value
+        return embed_details
+
+    @staticmethod
+    @postgres_connection
+    def FakeEmbeds(connection):
+        cursor = connection.cursor()
+        sql_query = "SELECT key, value FROM embeds WHERE key LIKE 'fakeinfo_%'"
+        cursor.execute(sql_query)
+        rows = cursor.fetchall()
+        embed_details = {}
+        for row in rows:
+            key, value = row
+            embed_details[key] = value
+        return embed_details
+
 
 def setup():
-
-    def create_table_embed(cursor):
-        """Create table if it does not exist"""
-        cursor.execute(
-            "SELECT EXISTS (SELECT FROM information_schema.tables WHERE table_name = 'embeds')"
-        )
-        table_exists = cursor.fetchone()[0]
-        if not table_exists:
-            cursor.execute(
-                "CREATE TABLE embeds (key VARCHAR(255) PRIMARY KEY, value TEXT);"
-            )
-            print("Table 'embeds' created successfully.")
-
-    def setup_table_embed():
-        """Load `embeds` table with defaults"""
-
-        # pleasewait table
-        Embeds.add("pleasewait_author_name", "NeaBot")
-        Embeds.add(
-            "pleasewait_author_url",
-            "https://cdn.discordapp.com/attachments/881007500588089404/881046764206039070/unknown.png",
-        )
-        Embeds.add("Pleasewait_title", "Please Wait")
-        Embeds.add("pleasewait_description", "``` Processing Your Request ```")
-        Embeds.add("pleasewait_color", "red")
-        Embeds.add(
-            "pleasewait_thumbnail",
-            "https://c.tenor.com/I6kN-6X7nhAAAAAj/loading-buffering.gif",
-        )
-        Embeds.add("pleasewait_footer", "Bot created by @hirushaadi")
-
-        # error
-        Embeds.add("error_title", "NeaBot")
-        Embeds.add("error_description", "The command was unable to run successfully!")
-        Embeds.add(
-            "error_thumbnail",
-            "https://cdn.discordapp.com/attachments/877796755234783273/879298565380386846/sign-red-error-icon-1.png",
-        )
-        Embeds.add("error_feild_name", "Error:")
-        Embeds.add("error_color", "red")
-
-        # fake embeds for `near.cogs.fakeinfo`
-        Embeds.add("fakeinfo_title", "Fake Information Generator")
-        Embeds.add(
-            "fakeinfo_thumbnail",
-            "https://www.nicepng.com/png/detail/214-2146883_4-fake-stamp-vector-fake-news-logo-png.png",
-        )
-        Embeds.add("fakeinfo_color", "red")
-
-        # common
-        Embeds.add("common_color", "red")
-
     try:
-
         connection = connect_to_postgres()
-
         if connection:
             cursor = connection.cursor()
-            create_table_embed(cursor=cursor)
-            setup_table_embed()
+            Embeds.__create_table_embed(cursor=cursor)
+            Embeds.__setup_table_embed()
 
     except psycopg2.Error as e:
         print("Error raised by PostgreSQL:", e)
 
     finally:
-        # Close the connection
         if connection:
             cursor.close()
             connection.close()
             print("PostgreSQL connection is closed.")
 
 
-setup()
+if __name__ == "__main__":
+    setup()
