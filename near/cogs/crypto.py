2ef03bfb9bb1291991e1eb7b6fcec5915dad714d
hirusha-adi <36286877+hirusha-adi@users.noreply.github.com>
2024-02-27 18:49:30 +0530

    format

--- near/cogs/crypto.py
+++ near/cogs/crypto.py
@@ -4,6 +4,7 @@ from discord.ext import commands
 
 from .cmnds.crypto import Cmnds_Crypto
 
+
 class Crypto(commands.Cog):
     def __init__(self, client: commands.Bot):
         self.client = client
@@ -15,19 +16,19 @@ class Crypto(commands.Cog):
     @app_commands.command(name="eth", description="Get the current Etherium Rates")
     async def eth(self, interaction: discord.Interaction):
         await Cmnds_Crypto.eth(interaction=interaction)
-        
+
     @app_commands.command(name="xmr", description="Get the current Monero Rates")
     async def xmr(self, interaction: discord.Interaction):
         await Cmnds_Crypto.xmr(interaction=interaction)
-            
+
     @app_commands.command(name="doge", description="Get the current Doge Coin Rates")
     async def doge(self, interaction: discord.Interaction):
         await Cmnds_Crypto.doge(interaction=interaction)
-            
+
     @app_commands.command(name="xrp", description="Get the current XRP Rates")
     async def xrp(self, interaction: discord.Interaction):
         await Cmnds_Crypto.xrp(interaction=interaction)
-            
+
     @app_commands.command(name="rvn", description="Get the current Raven Coin Rates")
     async def rvn(self, interaction: discord.Interaction):
         await Cmnds_Crypto.rvn(interaction=interaction)
