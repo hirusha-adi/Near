e592ba69d4d399f1919522890f36b33949147e08
Hirusha Adikari <36286877+hirusha-adi@users.noreply.github.com>
2024-04-10 19:29:01 +0530

    custom approach for embeds

--- near/utils/embeds.py
+++ near/utils/embeds.py
@@ -1,11 +1,38 @@
+from datetime import datetime
+import typing as t
+
 import discord
 from discord.ext import commands
+
 from near.database import get_embeds
 
-def Error(interaction: discord.Interaction, client: commands.Bot, error_message: str):
-    embed = discord.Embed(title=get_embeds.ErrorEmbeds.TITLE, description=get_embeds.ErrorEmbeds.DESCRIPTION, color=get_embeds.ErrorEmbeds.COLOR)
+
+def Error(client: commands.Bot, interaction: discord.Interaction, error_message: str) -> discord.Embed:
+    embed = discord.Embed(title=get_embeds.ErrorEmbeds.TITLE, description=get_embeds.ErrorEmbeds.DESCRIPTION, color=get_embeds.ErrorEmbeds.COLOR, timestamp=datetime.utcnow())
     embed.set_author(name=f"{client.user.name}", icon_url=f"{client.user.avatar.url}")
     embed.set_thumbnail(url=get_embeds.ErrorEmbeds.THUMBNAIL)
     embed.add_field(name=get_embeds.ErrorEmbeds.FIELD_NAME, value=f"{error_message}", inline=False)
+    embed.set_footer(text=f"Requested by {interaction.user.name}")
+    return embed
+
+def Common(client: commands.Bot, interaction: discord.Interaction, title: str, description: t.Optional[str | list | bool] = False, thumbnail: t.Optional[str] = "",) -> discord.Embed:
+    if description == False:  # dont add description
+        embed = discord.Embed(
+            title=title,
+            color=get_embeds.Common.COLOR,
+            timestamp=datetime.utcnow()
+        )
+    else:                    # add description
+        embed = discord.Embed(
+            title=title,
+            description=(
+                "".join(description) if isinstance(description, list) else description
+            ),
+            color=get_embeds.Common.COLOR,
+            timestamp=datetime.utcnow()
+        )
+    embed.set_author(name=client.user.name, icon_url=client.user.avatar.url)
+    if thumbnail:
+        embed.set_thumbnail(url=thumbnail)
     embed.set_footer(text=f"Requested by {interaction.user.name}")
     return embed
