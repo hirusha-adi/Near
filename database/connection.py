af097ae1d2b9d7fc1929e088091da8aa4d47de3b
hirusha-adi <36286877+hirusha-adi@users.noreply.github.com>
2024-04-10 16:49:53 +0530

    normal and async connectors

--- database/connection.py
+++ database/connection.py
@@ -1,6 +1,7 @@
 import os
 import psycopg2
-from psycopg2 import Error
+import asyncpg
+
 
 def psql_connection():
     def decorator(func):
@@ -10,12 +11,35 @@ def psql_connection():
                     user=os.getenv("PSQL_USER"),
                     password=os.getenv("PSQL_PASSWORD"),
                     host=os.getenv("PSQL_HOST"),
-                    database=os.getenv("PSQL_DATABASE")
+                    database=os.getenv("PSQL_DATABASE"),
                 )
                 result = func(connection, *args, **kwargs)
                 connection.close()
                 return result
-            except (Exception, Error) as error:
+            except (Exception, psycopg2.Error) as error:
                 print("Error while connecting to PostgreSQL:", error)
+
         return wrapper
+
+    return decorator
+
+
+def psql_connection_async():
+    async def decorator(func):
+        async def wrapper(*args, **kwargs):
+            try:
+                connection = await asyncpg.connect(
+                    user=os.getenv("PSQL_USER"),
+                    password=os.getenv("PSQL_PASSWORD"),
+                    host=os.getenv("PSQL_HOST"),
+                    database=os.getenv("PSQL_DATABASE"),
+                )
+                result = await func(connection, *args, **kwargs)
+                await connection.close()
+                return result
+            except (Exception, asyncpg.PostgresError) as error:
+                print("Error while connecting to PostgreSQL:", error)
+
+        return wrapper
+
     return decorator
