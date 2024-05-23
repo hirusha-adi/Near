0034304f90304a8ad8faf756dba8772490615f8a
Hirusha Adikari <36286877+hirusha-adi@users.noreply.github.com>
2024-05-24 00:00:21 +0530

    keep on adding stuff

--- near/cogs/general.py
+++ near/cogs/general.py
@@ -7,6 +7,7 @@ from discord import app_commands
 from discord.ext import commands
 
 from near.database import get_embeds
+from near.utils import embeds
 
 
 class Select(discord.ui.Select):
@@ -151,37 +152,25 @@ class General(commands.Cog):
     @commands.Cog.listener()
     async def on_command_error(self, ctx, error):
         if isinstance(error, commands.MissingPermissions):
-            embed = discord.Embed(title="ERROR", description="`You don't have the permissions required to use this command!`", color=get_embeds.Common.COLOR)
-            embed.set_author(name=f"{self.client.user.name}", icon_url=f"{self.client.user.avatar.url}")
-            embed.set_thumbnail(url="https://cdn.discordapp.com/attachments/877796755234783273/879298565380386846/sign-red-error-icon-1.png")
-            await ctx.send(embed=embed)
-            return
+            await ctx.send(embed=embeds.Error(interaction=ctx, client=self.client, error_message=f"You don't have the permissions required to use this command!"), ephemeral=False)
 
         if isinstance(error, commands.MissingRequiredArgument):
-            embed = discord.Embed(title="Something is wrong!", description="An error has been occured!", color=get_embeds.Common.COLOR)
-            embed.set_author(name=f"{self.client.user.name}", icon_url=f"{self.client.user.avatar.url}")
-            embed.set_thumbnail(url="https://cdn.discordapp.com/attachments/877796755234783273/879298565380386846/sign-red-error-icon-1.png")
-            embed.add_field(name="Error", value="You haven't passed the needed arguments for this command to run properly", inline=True)
-            embed.add_field(name="Possible Fix", value=f"use `/help` to list out all the command and check the proper usage of the command you used", inline=True)
-            await ctx.send(embed=embed)
-            return
+            await ctx.send(embed=embeds.Error(interaction=ctx, client=self.client, error_message=f"You haven't passed the needed arguments for this command to run properly.\n\nPlease use `/help` to list out all the command and check the proper usage of the command you used."), ephemeral=False)
 
     @app_commands.command(name="ping", description="Check the response time of the Discord Bot")
     async def ping(self, interaction: discord.Interaction):
         try:
-            embed = discord.Embed(title=":timer:  Response Time  :timer:", description=f"**{round(self.client.latency * 1000)} ms**", color=get_embeds.Common.COLOR)
-            embed.set_author(name=f"{self.client.user.name}", icon_url=f"{self.client.user.avatar.url}")
-            embed.set_thumbnail(url="https://cdn.discordapp.com/attachments/877796755234783273/879311068097290320/PngItem_1526969.png")
-            embed.set_footer(text=f"Requested by {interaction.user.name}")
+            embed = embeds.Common(
+                client=self.client,
+                interaction=interaction,
+                title=":timer:  Response Time  :timer:",
+                description=f"**{round(self.client.latency * 1000)} ms**",
+                thumbnail="https://cdn.discordapp.com/attachments/877796755234783273/879311068097290320/PngItem_1526969.png"
+            )
             await interaction.response.send_message(embed=embed)
 
         except Exception as e:
-            embed3 = discord.Embed(title=get_embeds.ErrorEmbeds.TITLE, description=get_embeds.ErrorEmbeds.DESCRIPTION, color=get_embeds.ErrorEmbeds.COLOR)
-            embed3.set_author(name=f"{self.client.user.name}", icon_url=f"{self.client.user.avatar.url}")
-            embed3.set_thumbnail(url=get_embeds.ErrorEmbeds.THUMBNAIL)
-            embed3.add_field(name=get_embeds.ErrorEmbeds.FIELD_NAME, value=f"{e}", inline=False)
-            embed3.set_footer(text=f"Requested by {interaction.user.name}")
-            await interaction.response.send_message(embed=embed3, ephemeral=True)
+            await interaction.response.send_message(embed=embeds.Error(interaction=interaction, client=self.client, error_message=f"{e}"), ephemeral=False)
 
     @app_commands.command(name="uptime", description="How long has the bot been up for?")
     async def uptime(self, interaction: discord.Interaction):
@@ -194,12 +183,7 @@ class General(commands.Cog):
             await interaction.response.send_message(embed=embed)
 
         except Exception as e:
-            embed3 = discord.Embed(title=get_embeds.ErrorEmbeds.TITLE, description=get_embeds.ErrorEmbeds.DESCRIPTION, color=get_embeds.ErrorEmbeds.COLOR)
-            embed3.set_author(name=f"{self.client.user.name}", icon_url=f"{self.client.user.avatar.url}")
-            embed3.set_thumbnail(url=get_embeds.ErrorEmbeds.THUMBNAIL)
-            embed3.add_field(name=get_embeds.ErrorEmbeds.FIELD_NAME, value=f"{e}", inline=False)
-            embed3.set_footer(text=f"Requested by {interaction.user.name}")
-            await interaction.response.send_message(embed=embed3, ephemeral=True)
+            await interaction.response.send_message(embed=embeds.Error(interaction=interaction, client=self.client, error_message=f"{e}"), ephemeral=False)
 
     @app_commands.command(name="clean", description="Delete messages sent by the Bot")
     @app_commands.describe(amount="Amount of messages to Delete")
@@ -221,20 +205,10 @@ class General(commands.Cog):
                 await interaction.response.send_message(embed=embed, ephemeral=True)
 
             else:
-                embed2 = discord.Embed(title=get_embeds.ErrorEmbeds.TITLE, description=get_embeds.ErrorEmbeds.DESCRIPTION, color=get_embeds.ErrorEmbeds.COLOR)
-                embed2.set_author(name=f"{self.client.user.name}", icon_url=f"{self.client.user.avatar.url}")
-                embed2.set_thumbnail(url=get_embeds.ErrorEmbeds.THUMBNAIL)
-                embed2.add_field(name="Error:", value=f"Please enter a value below 100!", inline=False)
-                embed2.set_footer(text=f"Requested by {interaction.user.name}")
-                await interaction.response.send_message(embed=embed2, ephemeral=True)
+                await interaction.response.send_message(embed=embeds.Error(interaction=interaction, client=self.client, error_message=f"Please enter a value below 100!"), ephemeral=False)
 
         except Exception as e:
-            embed3 = discord.Embed(title=get_embeds.ErrorEmbeds.TITLE, description=get_embeds.ErrorEmbeds.DESCRIPTION, color=get_embeds.ErrorEmbeds.COLOR)
-            embed3.set_author(name=f"{self.client.user.name}", icon_url=f"{self.client.user.avatar.url}")
-            embed3.set_thumbnail(url=get_embeds.ErrorEmbeds.THUMBNAIL)
-            embed3.add_field(name=get_embeds.ErrorEmbeds.FIELD_NAME, value=f"{e}", inline=False)
-            embed3.set_footer(text=f"Requested by {interaction.user.name}")
-            await interaction.response.send_message(embed=embed, ephemeral=True)
+            await interaction.response.send_message(embed=embeds.Error(interaction=interaction, client=self.client, error_message=f"{e}"), ephemeral=False)
 
     @app_commands.command(name="help", description="Command Support")
     async def help(self, interaction: discord.Interaction):
@@ -252,13 +226,7 @@ class General(commands.Cog):
             await interaction.response.send_message(embed=embed3, view=SelectView(), ephemeral=False)
 
         except Exception as e:
-            embed3 = discord.Embed(title=get_embeds.ErrorEmbeds.TITLE, description=get_embeds.ErrorEmbeds.DESCRIPTION, color=get_embeds.ErrorEmbeds.COLOR)
-            embed3.set_author(name=f"{self.client.user.name}", icon_url=f"{self.client.user.avatar.url}")
-            embed3.set_thumbnail(url=get_embeds.ErrorEmbeds.THUMBNAIL)
-            embed3.add_field(name=get_embeds.ErrorEmbeds.FIELD_NAME, value=f"{e}", inline=False)
-            embed3.set_footer(text=f"Requested by {interaction.user.name}")
-
-            await interaction.response.send_message(embed=embed3, ephemeral=True)
+            await interaction.response.send_message(embed=embeds.Error(interaction=interaction, client=self.client, error_message=f"{e}"), ephemeral=False)
 
 
 async def setup(client: commands.Bot):
