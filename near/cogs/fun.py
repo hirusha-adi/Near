ad2571c5b6dc928979ff861dd17f1e8110590837
Hirusha Adikari <36286877+hirusha-adi@users.noreply.github.com>
2024-05-23 23:44:06 +0530

    use aiohttp

--- near/cogs/fun.py
+++ near/cogs/fun.py
@@ -1,9 +1,8 @@
 import random
-from json import loads as loadjsonstring
 
 import aiohttp
 import discord
-import requests
+import json
 from bs4 import BeautifulSoup
 from discord import app_commands
 from discord.ext import commands
@@ -18,9 +17,10 @@ class Fun(commands.Cog):
     @app_commands.command(name="inspire", description="Get an inspirational quote")
     async def inspire(self, interaction: discord.Interaction):
         try:
-            r = requests.get("https://zenquotes.io/api/random")
-            json_data = loadjsonstring(r.text)
-            quote = json_data[0]['q'] + " - " + json_data[0]['a']
+            async with aiohttp.ClientSession() as session:
+                async with session.get("https://zenquotes.io/api/random") as response:
+                    json_data = await response.json()
+                    quote = json_data[0]['q'] + " - " + json_data[0]['a']
 
             embed = embeds.Common(
                 client=self.client,
@@ -38,16 +38,17 @@ class Fun(commands.Cog):
     @app_commands.command(name="meme", description="Get a random meme")
     async def meme(self, interaction: discord.Interaction):
         try:
-            url = 'https://www.memedroid.com/memes/tag/programming'
-            response = requests.get(url)
-            soup = BeautifulSoup(response.content, 'html.parser')
-            divs = soup.find_all('div', class_='item-aux-container')
-            imgs = []
-            for div in divs:
-                img = div.find('img')['src']
-                if img.startswith('http') and img.endswith('jpeg'):
-                    imgs.append(img)
-            meme = random.choice(imgs)
+            async with aiohttp.ClientSession() as session:
+                async with session.get('https://www.memedroid.com/memes/tag/programming') as response:
+                    html_content = await response.text()
+                    soup = BeautifulSoup(html_content, 'html.parser')
+                    divs = soup.find_all('div', class_='item-aux-container')
+                    imgs = []
+                    for div in divs:
+                        img = div.find('img')['src']
+                        if img.startswith('http') and img.endswith('jpeg'):
+                            imgs.append(img)
+                    meme = random.choice(imgs)
             
             embed = embeds.Common(
                 client=self.client,
@@ -63,12 +64,8 @@ class Fun(commands.Cog):
     @app_commands.command(name="dadjoke", description="Get a random dad joke")
     async def dadjoke(self, interaction: discord.Interaction):
         try:
-            headers = {
-                "Accept": "application/json"
-            }
-
             async with aiohttp.ClientSession() as session:
-                async with session.get("https://icanhazdadjoke.com", headers=headers) as req:
+                async with session.get("https://icanhazdadjoke.com", headers={"Accept": "application/json"}) as req:
                     r = await req.json()
             
             embed = embeds.Common(
@@ -87,12 +84,13 @@ class Fun(commands.Cog):
     @app_commands.command(name="wyr", description="Would You Rather...?")
     async def wyr(self, interaction: discord.Interaction):
         try:
-            r = requests.get('https://www.conversationstarters.com/wyrqlist.php').text
-
-            soup = BeautifulSoup(r, 'html.parser')
-            qa = soup.find(id='qa').text
-            qor = soup.find(id='qor').text
-            qb = soup.find(id='qb').text
+            async with aiohttp.ClientSession() as session:
+                async with session.get('https://www.conversationstarters.com/wyrqlist.php') as response:
+                    html_content = await response.text()
+                    soup = BeautifulSoup(html_content, 'html.parser')
+                    qa = soup.find(id='qa').text
+                    qor = soup.find(id='qor').text
+                    qb = soup.find(id='qb').text
             
             embed = embeds.Common(
                 client=self.client,
@@ -109,14 +107,17 @@ class Fun(commands.Cog):
     @app_commands.command(name="advice", description="Get advice for your life")
     async def advice(self, interaction: discord.Interaction):
         try:
-            r = requests.get("https://api.adviceslip.com/advice").json()
-            c = r['slip']['advice']
+            async with aiohttp.ClientSession() as session:
+                async with session.get("https://api.adviceslip.com/advice") as response:
+                    data_str = await response.text()
+                    data_json = json.loads(data_str)
+                    c = data_json['slip']['advice']
 
             embed = embeds.Common(
                 client=self.client,
                 interaction=interaction,
                 title="an Advice",
-                description=str(c),
+                description=c,
                 thumbnail="https://cdn.discordapp.com/attachments/877796755234783273/880034306720956456/download_1.jfif"
             )
             await interaction.response.send_message(embed=embed)
@@ -124,16 +125,20 @@ class Fun(commands.Cog):
         except Exception as e:
             await interaction.response.send_message(embed=embeds.Error(interaction=interaction, client=self.client, error_message=f"{e}"), ephemeral=False)
 
-    @app_commands.command(name="joke2", description="Get a Joke, but from Another API")
+    @app_commands.command(name="joke", description="Get a Joke, but from Another API")
     async def joke(self, interaction: discord.Interaction):
         try:
-            r = requests.get("https://some-random-api.ml/joke").json()
+            url = "https://some-random-api.ml/joke"
+            async with aiohttp.ClientSession() as session:
+                async with session.get(url) as response:
+                    json_txt = await response.text()
+                    json_data = json.loads(json_txt)
 
             embed = embeds.Common(
                 client=self.client,
                 interaction=interaction,
                 title="a Joke",
-                description=f"{r['joke']}",
+                description=f"{json_data['joke']}",
                 thumbnail="https://media.discordapp.net/attachments/877796755234783273/880742956552822794/mr-bean-avatar-character-cartoon-rowan-atkinson-png-image-33.png?width=454&height=584"
             )
             await interaction.response.send_message(embed=embed)
