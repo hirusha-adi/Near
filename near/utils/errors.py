91d03aa43e2a36de1e9da3e818b5086c1f03d1ef
Hirusha Adikari <hirushaadi@gmail.com>
2024-08-29 05:54:31 +0530

    `CommandError` - raised inside a command to be catched below

--- near/utils/errors.py
+++ near/utils/errors.py
@@ -2,9 +2,18 @@ def handle_command_errors(func):
     # TO BE COMPLETED LATER
     ...
 
+
 class IllegalInput(Exception):
     def __init__(self, message: str = None):
         self.message = message
 
     def __str__(self):
         return f"Illegal Input Passed to the bot as an argument{': ' + str(self.message) if not(self.message is None) else '.'}"
+
+
+class CommandError(Exception):
+    def __init__(self, message: str = None):
+        self.message = message
+
+    def __str__(self):
+        return f"Error running the command{': ' + str(self.message) if not(self.message is None) else '.'}"
