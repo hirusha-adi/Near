5f8787b05b39294f2725145a6f09396ae359dd1e
Hirusha Adikari <hirushaadi@gmail.com>
2024-10-28 22:47:41 +0530

    models

---
+++ near/database/models.py
@@ -0,0 +1,13 @@
+from tortoise import fields, models
+
+class DataGeneral(models.Model):
+    key = fields.CharField(pk=True, max_length=255)  # Primary key
+    value = fields.TextField()                       # Text value
+
+class DataAdBroadcast(models.Model):
+    key = fields.CharField(pk=True, max_length=255)  # Primary key
+    value = fields.TextField()                       # Text value
+
+class DataEmbeds(models.Model):
+    key = fields.CharField(pk=True, max_length=255)  # Primary key
+    value = fields.TextField()                       # Text value
