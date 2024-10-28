1a0a86c164fcb6e0cbe27a7d0410e7aac3e29c27
Hirusha Adikari <hirushaadi@gmail.com>
2024-10-28 20:33:47 +0530

    use numpy  style docs

--- near/utils/input_sanitization.py
+++ near/utils/input_sanitization.py
@@ -11,13 +11,15 @@ def check_input(input_str: str) -> bool:
     """
     Check if input string contains any potentially harmful commands.
 
-    Args:
-        input_str (str): The string to check.
+    Parameters
+    ----------
+    input_str : str
+        The string to check.
 
-    Returns:
-        bool: 
-            True if the string is safe, 
-            False if it contains a harmful command.
+    Returns
+    -------
+    bool
+        True if the string is safe, False if it contains a harmful command.
     """
     
     for keyword in privilege_escalation_keywords:
@@ -29,15 +31,17 @@ def check_input(input_str: str) -> bool:
 def is_base64(input_str: str) -> bool:
     """
     Check if input string is a valid base64 string.
-    Based on: https://stackoverflow.com/questions/475074/regex-to-parse-or-validate-base64-data
 
-    Args:
-        input_str (str): The string to check.
+    Parameters
+    ----------
+    input_str : str
+        The string to check.
 
-    Returns:
-        bool: 
-            True if the string is a valid base64 string,
-            False if it is not.
+    Returns
+    -------
+    bool
+        True if the string is a valid base64 string,
+        False if it is not.
     """
 
     pattern = r'^(?:[A-Za-z0-9+/]{4})*(?:[A-Za-z0-9+/]{2}==|[A-Za-z0-9+/]{3}=)?$'
@@ -46,19 +50,19 @@ def is_base64(input_str: str) -> bool:
 
 def is_ipaddr(input_str: str) -> bool:
     """
-    Checks if input string is a valid IP address (both IPv4 and IPv6).
-    Based on: 
-        https://gist.github.com/hirusha-adi/5ed5000246e16dfa035ea604362c763f
-        https://docs.python.org/3/library/ipaddress.html#ipaddress.ip_address
+    Check if input string is a valid IP address (both IPv4 and IPv6).
 
-    Args:
-        input_str (str): The string to check.
+    Parameters
+    ----------
+    input_str : str
+        The string to check.
 
-    Returns:
-        bool: 
-            True if the string is a valid IP address, 
-            False otherwise.
+    Returns
+    -------
+    bool
+        True if the string is a valid IP address, False otherwise.
     """
+    
     try:
         ipaddress.ip_address(input_str)
         return True
@@ -69,13 +73,16 @@ def is_text_only(input_str: str) -> bool:
     """
     Check if the input string contains only alphabetic characters.
 
-    Args:
-        input_str (str): The string to check.
+    Parameters
+    ----------
+    input_str : str
+        The string to check.
 
-    Returns:
-        bool: 
-            True if the string contains only alphabetic characters,
-            False otherwise.
+    Returns
+    -------
+    bool
+        True if the string contains only alphabetic characters,
+        False otherwise.
     """
 
     pattern = r'^[a-zA-Z]+$'
@@ -85,17 +92,21 @@ def is_text_only(input_str: str) -> bool:
 def is_instagram_username(input_str: str) -> bool:
     """
     Check if the input string is a valid Instagram username.
+
     Valid Instagram usernames:
-        - Must be 1-29 characters long
-        - Must only contain alphanumeric characters
+    - Must be 1-29 characters long
+    - Must only contain alphanumeric characters
 
-    Args:
-        input_str (str): The string to check.
+    Parameters
+    ----------
+    input_str : str
+        The string to check.
 
-    Returns:
-        bool: 
-            True if the string is a valid Instagram username,
-            False otherwise.
+    Returns
+    -------
+    bool
+        True if the string is a valid Instagram username,
+        False otherwise.
     """
 
     pattern = r'^[a-zA-Z0-9]{1,29}$'
@@ -110,17 +121,22 @@ def password_check(input_str):
 
 def color(hex: str) -> int:
     """
-    Convert a given hex string to an integer.
+    Converts a given hex color code to an integer.
 
-    Args:
-        hex (str): The string to convert.
+    Parameters
+    ----------
+    hex : str
+        The hex color code to convert.
 
-    Returns:
-        int: 
-            The integer value of the given hex string.
-            If the conversion fails, return the default color red (int: 0xFF0000).
+    Returns
+    -------
+    int
+        The integer representation of the given hex color code.
     """
+    
     try:
+        # Attempt to convert the hex color code to an integer.
         return int(hex, 16)
     except ValueError:
-        return 0xFF0000  # return default color red
+        # If the conversion fails, return the default color red.
+        return 0xFF0000
