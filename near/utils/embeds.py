fed6bc80d48e62fb211391c368b61363cb7f1eb8
Hirusha Adikari <hirushaadi@gmail.com>
2024-10-31 08:26:17 -0400

    fetch embeds from the DB without errors

--- near/utils/embeds.py
+++ near/utils/embeds.py
@@ -9,10 +9,13 @@ from near.database import get_embeds
 from near.database import dbfetch
 from near.utils import input_sanitization
 
-async def Error(client: commands.Bot, interaction: discord.Interaction, error_message: str) -> discord.Embed:
+
+async def Error(
+    client: commands.Bot, interaction: discord.Interaction, error_message: str
+) -> discord.Embed:
     """
     Generate an error embed given an error message.
-    
+
     Parameters:
     -----------
     client: commands.Bot
@@ -21,25 +24,38 @@ async def Error(client: commands.Bot, interaction: discord.Interaction, error_me
         The interaction which invoked the error.
     error_message: str
         The error message to be embedded and sent.
-    
+
     Returns:
     --------
     discord.Embed
         An embed containing the error message and the interaction user.
     """
-    
+
     logger.error(error_message)
     embed_info = await dbfetch.DataEmbedsFetcher.allErrorVals()
-    embed = discord.Embed(title=embed_info['ERROR_TITLE'], description=f"```\n{error_message}```", color=input_sanitization.color(embed_info['ERROR_COLOR']), timestamp=datetime.utcnow())
+    embed = discord.Embed(
+        title=embed_info["ERROR_TITLE"],
+        description=f"```\n{error_message}```",
+        color=input_sanitization.color(embed_info["ERROR_COLOR"]),
+        timestamp=datetime.utcnow(),
+    )
     embed.set_author(name=f"{client.user.name}", icon_url=f"{client.user.avatar.url}")
-    embed.set_thumbnail(url=embed_info['ERROR_THUMBNAIL'])
+    embed.set_thumbnail(url=embed_info["ERROR_THUMBNAIL"])
     embed.set_footer(text=f"Requested by {interaction.user.name}")
     return embed
 
-async def Common(client: commands.Bot, interaction: discord.Interaction, title: str, description: t.Optional[str | list | bool] = False, thumbnail: t.Optional[str] = "", thumbnail_direct: t.Optional[bool] = False) -> discord.Embed:
+
+async def Common(
+    client: commands.Bot,
+    interaction: discord.Interaction,
+    title: str,
+    description: t.Optional[str | list | bool] = False,
+    thumbnail: t.Optional[str] = "",
+    thumbnail_direct: t.Optional[bool] = False,
+) -> discord.Embed:
     """
     Create a common embed with a title and optional description and thumbnail.
-    
+
     Parameters
     ----------
     client : commands.Bot
@@ -52,35 +68,42 @@ async def Common(client: commands.Bot, interaction: discord.Interaction, title: 
         The description of the embed. If it's a list, the elements are joined.
         If False, no description is added. Defaults to False.
     thumbnail : str, optional
-        URL of the thumbnail image to be added to the embed. Defaults to an empty string.
-    
+        Defaults to an empty string.
+        If empty string, no thumbnail will set to the embed.
+        URL/Name of the thumbnail image to be added to the embed.
+    thumbnail_direct : bool, optional
+        Defaults to False.
+        If True, the thumbnail URL is used directly.
+        If False, it is used as a key to fetch the URL from the database.
+
     Returns
     -------
     discord.Embed
         An embed with the specified title, optional description, and thumbnail.
     """
-    
-    if description == False:            # dont add description
+
+    if description == False:  # dont add description
         embed = discord.Embed(
-            title=title,
-            color=get_embeds.Common.COLOR,
-            timestamp=datetime.utcnow()
+            title=title, color=get_embeds.Common.COLOR, timestamp=datetime.utcnow()
         )
-    else:                               # add description
+    else:  # add description
         embed = discord.Embed(
             title=title,
             description=(
                 "".join(description) if isinstance(description, list) else description
             ),
             color=get_embeds.Common.COLOR,
-            timestamp=datetime.utcnow()
+            timestamp=datetime.utcnow(),
         )
 
     if thumbnail:
         if thumbnail_direct:
             embed.set_thumbnail(url=thumbnail)
+            print(thumbnail_direct, thumbnail)
         else:
-            embed.set_thumbnail(url=await dbfetch.DataEmbedThumbnailsFetcher.oneVal(key=thumbnail))
+            embed.set_thumbnail(
+                url=await dbfetch.DataEmbedThumbnailsFetcher.oneVal(key=thumbnail)
+            )
 
     embed.set_author(name=client.user.name, icon_url=client.user.avatar.url)
     embed.set_footer(text=f"Requested by {interaction.user.name}")
