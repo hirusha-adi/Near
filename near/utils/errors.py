472cdefdb9cf5b970e932e1fb15fd70b5ba9a91d
Hirusha Adikari <hirushaadi@gmail.com>
2024-10-31 06:59:26 -0400

    add PermissionError + unwanted

--- near/utils/errors.py
+++ near/utils/errors.py
@@ -6,87 +6,34 @@ def handle_command_errors(func):
 class IllegalInput(Exception):
     """
     Exception raised for illegal input passed to commands.
-
-    Parameters
-    ----------
-    message : str
-        The message to be displayed with the error. If not provided, a default
-        message will be used.
-
-    Attributes
-    ----------
-    message : str
-        The message to be displayed with the error.
-
-    Notes
-    -----
-    This exception is raised when the input provided to a command is not valid.
     """
 
     def __init__(self, message: str = None):
-        """
-        Initialize the IllegalInput exception.
-
-        Parameters
-        ----------
-        message : str
-            The message to be displayed with the error. If not provided, a default
-            message will be used.
-        """
         self.message = message
 
     def __str__(self):
-        """
-        Return a string representation of the exception.
-
-        Returns
-        -------
-        str
-            A string representation of the exception.
-        """
         return f"Illegal Input Passed to the bot as an argument{': ' + str(self.message) if not(self.message is None) else '.'}"
 
 
 class CommandError(Exception):
     """
     Exception raised for errors while running a command.
-
-    Parameters
-    ----------
-    message : str, optional
-        The message to be displayed with the error. If not provided, a default
-        message will be used.
-
-    Attributes
-    ----------
-    message : str
-        The message to be displayed with the error.
-
-    Notes
-    -----
-    This exception is raised when the command itself is unable to run due to
-    some error.
     """
 
     def __init__(self, message: str = None):
-        """
-        Initialize the CommandError exception.
-
-        Parameters
-        ----------
-        message : str, optional
-            The message to be displayed with the error. If not provided, a default
-            message will be used.
-        """
         self.message = message
 
     def __str__(self):
-        """
-        Return a string representation of the exception.
-
-        Returns
-        -------
-        str
-            A string representation of the exception.
-        """
         return self.message or "Unable to run the command"
+
+
+class PermissionError(Exception):
+    """
+    Exception raised for lack of permissions to run the command.
+    """
+
+    def __init__(self, message: str = None):
+        self.message = message
+
+    def __str__(self):
+        return self.message or "You do not have enough permissions to run this command!"  
