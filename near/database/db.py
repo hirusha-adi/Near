88313a0904f87c7f4c0872af8b0b8a971e7cd95b
Hirusha Adikari <hirushaadi@gmail.com>
2024-11-22 09:59:08 -0500

    exit if connection issue, else, bot will be bugged and wont work

--- near/database/db.py
+++ near/database/db.py
@@ -1,21 +1,27 @@
 import sys
+from loguru import logger
 from pocketbase import PocketBase
 from pocketbase.services.record_service import RecordService
-from loguru import logger
 
 DB_URL: str = "http://127.0.0.1:8090"
 POCKETBASE_AUTH_EMAIL: str = "hirushaadi@gmail.com"
 POCKETBASE_AUTH_PASSWORD: str = "11111111"
 
-conn = PocketBase(DB_URL)
+try:
+    conn = PocketBase(DB_URL)
 
-# Authentication
-user_data = conn.collection("users").auth_with_password(
-    POCKETBASE_AUTH_EMAIL, POCKETBASE_AUTH_PASSWORD
-)
-if not(user_data.is_valid):
-    sys.exit("Unable to authenticate with Pocketbase!")
-logger.success(f"Database authentication successful! Logged in as {user_data.record.id} ({user_data.record.name} - {user_data.record.email})")
+    # Authentication
+    user_data = conn.collection("users").auth_with_password(
+        POCKETBASE_AUTH_EMAIL, POCKETBASE_AUTH_PASSWORD
+    )
+    if not(user_data.is_valid):
+        logger.error("Unable to authenticate with Pocketbase!")
+        sys.exit()
+    logger.success(f"Database authentication successful! Logged in as {user_data.record.id} ({user_data.record.name} - {user_data.record.email})")
+
+except Exception as e:
+    logger.error(f"Unable to connect to Pocketbase! {e}")
+    sys.exit()
 
 class Collections:
     def settings_embeds() -> RecordService:
