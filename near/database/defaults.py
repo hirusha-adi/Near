7fc7b4a3352a2f137c7e5f8e6c7c084c72fef5e1
Hirusha Adikari <hirushaadi@gmail.com>
2024-11-22 09:58:26 -0500

    better logging + detect new keys in default config
    for easy updates

--- near/database/defaults.py
+++ near/database/defaults.py
@@ -1,19 +1,27 @@
 import json
 import os
+from loguru import logger
+
 from . import db
+
 
 async def set_defaults():
     await __set_defaultsSettingsEmbeds()
 
 
 async def __set_defaultsSettingsEmbeds():
-    settings_embeds_all = db.Collections.settings_embeds().get_full_list()
-    settings_embeds_all_keys = [x.key for x in settings_embeds_all]
-    if len(settings_embeds_all_keys) == 0:
-        try:
-            with open(os.path.join("near", "database", "default-data", "default.settings_embeds.json"), "r") as f:
-                default_data: dict = json.load(f)
-            # -------------------------------------
+    try:
+        # from the default config file
+        with open(os.path.join("near", "database", "default-data", "default.settings_embeds.json"), "r") as f:
+            default_data: dict = json.load(f)
+        default_data_keys = [x for x in default_data.keys()]
+        
+        # form pocketbase
+        settings_embeds_all = db.Collections.settings_embeds().get_full_list()
+        settings_embeds_all_keys = [x.key for x in settings_embeds_all]
+        
+        if len(settings_embeds_all_keys) == 0:
+            # if no existing data in database
             for key_default, val_default in default_data.items():
                 db.Collections.settings_embeds().create(
                     {
@@ -21,7 +29,25 @@ async def __set_defaultsSettingsEmbeds():
                         "value": val_default
                     }
                 )
-            # -------------------------------------
-            print("Default data loaded into DataEmbeds table.")
-        except Exception as e:
-            print(f"Failed to load default data into DataEmbeds: {e}")
+                logger.debug(f"Added key: {key_default}, value: {val_default}")
+            
+        elif len(default_data_keys) > len(settings_embeds_all_keys):
+            # means there has been an update in the default config
+            # in that case, add the new added record only
+            # without changing any other thing
+            missing_keys = set(default_data_keys) - set(settings_embeds_all_keys)
+            if missing_keys:
+                logger.debug(f"File `default.settings_embeds.json` has been updated! Adding new records.")
+                for missing_key in missing_keys:
+                    db.Collections.settings_embeds().create(
+                        {
+                            "key": missing_key,
+                            "value": val_default
+                        }
+                    )
+                    logger.debug(f"Added key: {missing_key}, value: {val_default}")
+        
+        logger.info("Default data loaded into settings_embeds collection.")
+                    
+    except Exception as e:
+        logger.error(f"Failed to load default data into settings_embeds: {e}")
